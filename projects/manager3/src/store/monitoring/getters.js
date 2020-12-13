3c5e312cc265d3f586cb27406cc1cafa13201198
Nicolas TESSIER <contact@nicolastessier.fr>
2020-12-13 15:35:28 +0100

    chore: work on realtime datas support over mqtt
    WIP : work on realtime datas support over mqtt

--- projects/manager3/src/store/monitoring/getters.js
+++ projects/manager3/src/store/monitoring/getters.js
@@ -2,35 +2,6 @@ export function storageInfo (state) {
   return state.storageinfo
 }
 
-export function temperatures (state) {
-  if (!state.metrics) return [
-    { data: [] }
-  ]
-
-  return state.metrics.temperature.temperatures
-}
-
-export function cores (state) {
-  if (!state.metrics) return [
-    { data: [{x: 'Core 1', y: 0 }, {x: 'Core 2', y: 0 }, {x: 'Core 3', y: 0 }, {x: 'Core 4', y: 0 }] }
-  ]
-
-  let cores = [
-    { data: [] }
-  ]
-
-  Object.keys(state.metrics.cpus).map(key => {
-    cores[0].data.push(
-      {
-        x: 'Core ' + (parseInt(key) + 1),
-        y: state.metrics.cpus[key].consumption[0].toFixed(0)
-      }
-    )
-  })
-
-  return cores
-}
-
-export function storages (state) {
-
+export function metrics (state) {
+  return state.metrics
 }

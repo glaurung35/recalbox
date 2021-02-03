808f972eb306ac4a257d5e98a6dbf1626cd4fbaa
Nicolas TESSIER <contact@nicolastessier.fr>
2021-02-03 22:10:07 +0100

    chore: storage charts integration

--- projects/manager3/src/pages/Monitoring.vue
+++ projects/manager3/src/pages/Monitoring.vue
@@ -8,9 +8,20 @@
         <apexchart :options="coresOptions" :series="cores" height="350" type="bar"></apexchart>
       </div>
       <div class="disks-usage col">
-        <q-linear-progress size="25px" :value="0.35" color="accent">
-          <div class="absolute-full flex flex-center">
-            <q-badge color="white" text-color="primary" :label="progressLabel"/>
+        <q-linear-progress
+          v-for="storage in storages"
+          size="30px"
+          :value="storage.used"
+          color="accent"
+          class="q-mt-sm progress"
+          :key="storage.label"
+          track-color="white"
+        >
+          <div class="absolute-full flex items-center">
+            <q-icon :name="storage.icon" color="white"/>
+            <div class="progress-text">
+              <span>{{ storage.deviceModel }} | <b>{{ storage.type }}</b> | {{ (storage.used * 100).toFixed(0) }}%</span>
+            </div>
           </div>
         </q-linear-progress>
       </div>
@@ -19,9 +30,9 @@
 </template>
 
 <script>
-import { date } from 'quasar'
+import { date, colors } from 'quasar'
 import VueApexCharts from 'vue-apexcharts'
-import { mapGetters } from 'vuex'
+import {mapGetters, mapState} from 'vuex'
 
 export default {
   name: 'Monitoring',
@@ -36,12 +47,35 @@ export default {
       cores: 'monitoring/cores',
       temperatures: 'monitoring/temperatures'
     }),
-    progressLabel() {
-      return '/sda1/share | ' + (0.35 * 100).toFixed(0) + '%'
-    }
   },
   data() {
     return {
+      storages: {
+        boot: {
+          icon: 'mdi-micro-sd',
+          deviceModel: 'SL32G',
+          type: 'boot',
+          used: 0.34
+        },
+        overlay: {
+          icon: 'mdi-micro-sd',
+          deviceModel: 'SL32G',
+          type: 'overlay',
+          used: .56
+        },
+        share: {
+          icon: 'mdi-usb-flash-drive',
+          deviceModel: 'SanDisk Extreme Pro',
+          type: 'share',
+          used: .75
+        },
+        network: {
+          icon: 'mdi-server-network',
+          deviceModel: 'Test',
+          type: 'test',
+          used: .45
+        },
+      },
       temperatureOptions: {
         chart: {
           type: 'line',
@@ -50,14 +84,14 @@ export default {
           toolbar: {show: false},
           zoom: {enabled: false},
         },
-        dataLabels: {enabled: false, style: {colors: ['#34495e']}, offsetX: 30},
+        dataLabels: {enabled: false, style: {colors: [colors.getBrand('primary')]}, offsetX: 30},
         tooltip: {enabled: false},
-        colors: ['#85d6de'],
-        stroke: {curve: 'smooth', show: true, width: 1, colors: ['#34495e']},
+        colors: [colors.getBrand('accent')],
+        stroke: {curve: 'smooth', show: true, width: 1, colors: [colors.getBrand('primary')]},
         fill: {type: 'gradient', gradient: {opacityFrom: 0.6, opacityTo: 0.8}},
         markers: {size: 0},
         yaxis: {
-          min: 0, max: 100, decimalsInFloat: 0, title: {text: 'CPU °C ( last 30s )', style: {color: '#34495e'}},
+          min: 0, max: 100, decimalsInFloat: 0, title: {text: 'CPU °C ( last 30s )', style: {color: colors.getBrand('primary')}},
         },
         xaxis: {
           labels: {
@@ -82,11 +116,11 @@ export default {
         responsive: [{breakpoint: 480}],
         xaxis: {type: 'category'},
         yaxis: {
-          title: {text: 'CPU CORES USAGE', style: {color: '#34495e'}},
+          title: {text: 'CPU CORES USAGE', style: {color: colors.getBrand('primary')}},
           min: 0,
           max: 100,
         },
-        colors: ['#85d6de'],
+        colors: [colors.getBrand('accent')],
         fill: {opacity: 0.8},
         legend: {show: false},
         tooltip: {enabled: false},
@@ -95,7 +129,7 @@ export default {
           formatter: function (val) {
             return val + "%"
           },
-          style: {fontSize: '12px', colors: ["#34495e"]},
+          style: {fontSize: '12px', colors: [colors.getBrand('primary')]},
         },
       },
     }
@@ -126,6 +160,26 @@ export default {
       flex: 1
 
     .disks-usage
-      flex: 2
+      flex: 1
       padding: .5em
+
+      .progress
+        border: 1px solid rgba(240, 240, 240, 1)
+
+        .q-linear-progress__model
+          opacity: .5
+
+        .progress-text
+          height: 100%
+          line-height: 28px
+          font-size: .4em
+          color: $primary
+
+          span
+            vertical-align: middle
+
+        i
+          font-size: .7em
+          margin-left: .2em
+          margin-right: .2em
 </style>

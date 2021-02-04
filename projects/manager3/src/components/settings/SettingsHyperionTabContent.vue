fbbf9fb66c9d97f4096b5ce6e95d78d054afda9c
Nicolas TESSIER <contact@nicolastessier.fr>
2021-02-04 23:54:30 +0100

    chore: ajoute la nouvelle feature hyperion

---
+++ projects/manager3/src/components/settings/SettingsHyperionTabContent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <div class="col col-xs-12 col-sm-12 col-md-12">
+      <FormFragmentContainer title="settings.hyperion.options.title">
+        <template v-slot:content>
+          <div class="col col-xs-12 col-sm-12 col-md-12">
+            <WrappedToggle
+              label="settings.hyperion.options.enableHyperion"
+              getter="hyperion/enabled"
+              setter="hyperion/post"
+              apiKey="enabled"
+            />
+          </div>
+        </template>
+      </FormFragmentContainer>
+    </div>
+  </div>
+</template>
+
+<script>
+import WrappedToggle from 'components/global/WrappedToggle'
+import FormFragmentContainer from '../global/FormFragmentContainer'
+
+export default {
+  name: 'SettingsHyperionTabContent',
+  components: {
+    WrappedToggle,
+    FormFragmentContainer,
+  },
+  created() {
+    this.$store.dispatch('hyperion/get')
+  },
+  data() {
+    return {
+      enableKodi: true,
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- projects/manager3/src/store/hyperion/getters.js
+++ projects/manager3/src/store/hyperion/getters.js
@@ -1,3 +1,7 @@
 export function _options(state) {
   return state._options
 }
+
+export function enabled (state) {
+  return state.enabled
+}

7730a0a06fa0984882a1872497a4b2471962e5b1
digitalLumberjack <digitallumberjack@gmail.com>
2022-10-20 00:11:34 +0200

    feat(system): lightgun: add support for guncon2 in lightgun configuration

--- projects/frontend/es-app/src/emulators/run/GameRunner.cpp
+++ projects/frontend/es-app/src/emulators/run/GameRunner.cpp
@@ -258,6 +258,13 @@ GameRunner::DemoRunGame(const FileData& game, const EmulatorData& emulator, int 
   command.append(" -demoduration ").append(Strings::ToString(duration));
   command.append(" -demoinfoduration ").append(Strings::ToString(infoscreenduration));
 
+  // Forced resolution
+  Resolutions::SimpleResolution targetResolution { 0, 0 };
+  if (ResolutionAdapter().AdjustResolution(0, RecalboxConf::Instance().GetSystemVideoMode(game.System()), targetResolution))
+    command.append(" -resolution ").append(targetResolution.ToString());
+
+  if (debug)
+    command.append(" -verbose");
 
   int exitCode = -1;
   {

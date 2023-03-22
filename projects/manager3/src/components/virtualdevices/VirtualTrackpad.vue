907cf8fefa2b2bb0246e4325ff74636e8bba1d5e
Nicolas TESSIER <nicolas.tessier@prestataire.sewan.fr>
2023-03-22 09:48:00 +0100

    feat(vu3): add historical Virtual Devices components

---
+++ projects/manager3/src/components/virtualdevices/VirtualTrackpad.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col col-xs-12 col-sm-12 col-md-12">
+      Trackpad
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="sass">
+
+</style>

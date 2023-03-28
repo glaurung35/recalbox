fa153c18fe51c5e7d6c76e9b1117989c9ecc25b2
Nicolas TESSIER <nicolas.tessier@prestataire.sewan.fr>
2023-03-28 17:04:07 +0200

    feat(vu3): zod + axios injection on versions
    feat(VU3): zod + axios injection on versions

--- projects/manager3/src/stores/versions.ts
+++ projects/manager3/src/stores/versions.ts
@@ -1,17 +1,15 @@
 import { defineStore } from 'pinia';
 import { VERSIONS } from 'src/router/api.routes';
+import { VersionsList } from 'stores/types/versionsList';
+
+export type VersionsStoreState = {
+  _baseUrl: string,
+  versions: VersionsList,
+};
 
 export const useVersionsStore = defineStore('versions', {
   state: () => ({
     _baseUrl: VERSIONS,
-    versions: {
-      linux: {},
-      recalbox: {},
-      webapi: {},
-      libretro: {
-        retroarch: '',
-        cores: {},
-      },
-    },
-  }),
+    versions: {},
+  } as VersionsStoreState),
 });

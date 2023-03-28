7717719890f1fb5c6210f2cf485148801bc70ce6
Nicolas TESSIER <nicolas.tessier@prestataire.sewan.fr>
2023-03-28 18:59:42 +0200

    feat(vu3): zod + axios injection on media

---
+++ projects/manager3/src/stores/types/mediasList.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const MediasListSchema = z.object({
+  mediaPath: z.string(),
+  mediaList: z.record(
+    z.string(),
+    z.object({
+      type: z.string(),
+    }),
+  ),
+});
+
+export type MediasList = z.infer<typeof MediasListSchema>

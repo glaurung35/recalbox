b92506990d62012e5e4d13bae67d8b0eea178d4c
Asthonishia <korx.klesk@gmail.com>
2023-03-30 17:39:48 +0200

    feat(vu3): adjust monitoring page

--- projects/manager3/src/components/monitoring/StoragesChartContent.vue
+++ projects/manager3/src/components/monitoring/StoragesChartContent.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="disks-usage col">
-    <q-linear-progress
-      v-for="item in items"
-      size="30px"
-      :value="item.used"
-      color="accent"
-      class="q-mt-sm progress"
-      :key="item.mount"
-      track-color="white"
-      rounded
-    >
-      <div class="absolute-full flex items-center">
-        <q-icon :name="item.icon" color="secondary"/>
-        <div class="progress-text">
-          <span>
-            {{ item.mount }} <b>| {{ (item.used * 100).toFixed(2) }}%</b>
-          </span>
-        </div>
+  <q-linear-progress
+    v-for="item in items"
+    size="30px"
+    :value="item.used"
+    color="accent"
+    class="q-mt-sm progress"
+    :key="item.mount"
+    track-color="white"
+    rounded
+  >
+    <div class="absolute-full flex items-center">
+      <q-icon :name="item.icon" color="secondary"/>
+      <div class="progress-text">
+        <span>
+          {{ item.mount }} <b>| {{ (item.used * 100).toFixed(2) }}%</b>
+        </span>
       </div>
-    </q-linear-progress>
-  </div>
+    </div>
+  </q-linear-progress>
 </template>
 
 <script lang="ts" setup>
@@ -48,29 +46,3 @@ const items = computed(() => {
   return result;
 });
 </script>
-
-<style lang="sass">
-.disks-usage
-  flex: 1
-  padding: .5em
-
-  .progress
-    border: 1px solid rgba(240, 240, 240, 1)
-
-    .q-linear-progress__model
-      opacity: .5
-
-    .progress-text
-      height: 100%
-      line-height: 28px
-      font-size: .4em
-      color: $primary
-
-      span
-        vertical-align: middle
-
-    i
-      font-size: .7em
-      margin-left: .2em
-      margin-right: .2em
-</style>
--- projects/manager3/src/pages/MonitoringPage.vue
+++ projects/manager3/src/pages/MonitoringPage.vue
@@ -2,8 +2,12 @@
   <q-page class="background monitoring">
     <CPUChartContent :data="JSON.parse(JSON.stringify(areaChartData))"/>
     <div class="charts-container row">
-      <CoresChartContent :data="cores"/>
-      <StoragesChartContent :data="storages"/>
+      <div class="col cores-usage">
+        <CoresChartContent :data="cores"/>
+      </div>
+      <div class="col disks-usage">
+        <StoragesChartContent :data="storages"/>
+      </div>
     </div>
   </q-page>
 </template>
@@ -45,4 +49,32 @@ const storages = computed(() => monitoring.value.storages);
     .col
       background: white
       margin: 0 8px 1em
+
+  .cores-usage
+    flex: 2
+    min-height: 508px
+
+  .disks-usage
+    flex: 1
+    padding: .5em
+
+    .progress
+      border: 1px solid rgba(240, 240, 240, 1)
+
+      .q-linear-progress__model
+        opacity: .5
+
+      .progress-text
+        height: 100%
+        line-height: 28px
+        font-size: .4em
+        color: $primary
+
+        span
+          vertical-align: middle
+
+      i
+        font-size: .7em
+        margin-left: .2em
+        margin-right: .2em
 </style>

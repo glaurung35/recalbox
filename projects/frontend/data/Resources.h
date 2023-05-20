25f2067cea08d72414be794f9af0cd123e24ba89
digitalLumberjack <digitallumberjack@gmail.com>
2023-05-20 10:41:04 +0200

    feat(frontend): rrgbj: add recalbox rgb jamma start logo

--- projects/frontend/data/Resources.h
+++ projects/frontend/data/Resources.h
@@ -146,6 +146,9 @@ extern const uint8_t crt__p640x480_png_data[];
 extern const uint16_t crt__i768x576_png_size;
 extern const uint8_t crt__i768x576_png_data[];
 
+extern const uint16_t crt__logojamma_svg_size;
+extern const uint8_t crt__logojamma_svg_data[];
+
 extern const uint16_t crt__p320x240_png_size;
 extern const uint8_t crt__p320x240_png_data[];
 

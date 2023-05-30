deba4abb8023f6ffeb91f93c94f6f89bc09c7de4
David Barbion <davidb@230ruedubac.fr>
2023-05-30 22:56:36 +0200

    refactor(image): generate list of boot files dynamically
    This commit modifies the script responsible of creating the image:
    * boot.md5 is generated before the image if generated
    * genimage.cfg of all boards is dynamically generated (list of files)
    
    Special case for the Odroid XU4 where bl1 boot file would overlap
    with bl2. genimage does not allow partition overlapping and will
    stop with an error. The bl1 file is truncated to 15360 bytes as
    recommended here https://lists.buildroot.org/pipermail/buildroot/2018-February/516384.html

--- package/pwm-audio-pi-zero/pwm-audio-pi-zero.mk
+++ package/pwm-audio-pi-zero/pwm-audio-pi-zero.mk
@@ -12,8 +12,8 @@ PWM_AUDIO_PI_ZERO_LICENSE_FILES = LICENSE
 PWM_AUDIO_PI_ZERO_DEPENDENCIES += rpi-firmware
 
 define PWM_AUDIO_PI_ZERO_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pi-zero-overlay.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pi-zero.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pi-zero-overlay.dts $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pi-zero.dts
+	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pi-zero-overlay.dtbo $(BINARIES_DIR)/boot-data/overlays/pwm-audio-pi-zero.dtbo
+	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pi-zero-overlay.dts $(BINARIES_DIR)/boot-data/overlays/pwm-audio-pi-zero.dts
 endef
 
 $(eval $(generic-package))

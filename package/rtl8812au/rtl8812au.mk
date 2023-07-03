caf46ca746d3526da873060fcdedfcc98a2c74bd
David Barbion <davidb@230ruedubac.fr>
2023-07-03 21:28:05 +0200

    feat(buildroot): bump rtl8812au wifi driver to v5.6.4.2

--- package/rtl8812au/rtl8812au.mk
+++ package/rtl8812au/rtl8812au.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-RTL8812AU_VERSION = 37e27f9165300c89607144b646545fac576ec510
+# commit of 3/07/2023 (v5.6.4.2)
+RTL8812AU_VERSION = 9dc4a4c9a2637df74692542aa01ba63f6fad6f5a
 RTL8812AU_SITE = $(call github,aircrack-ng,rtl8812au,$(RTL8812AU_VERSION))
 RTL8812AU_LICENSE = GPL-2.0
 RTL8812AU_LICENSE_FILES = LICENSE

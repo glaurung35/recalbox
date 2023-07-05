a4e0c92e541ad69072205bffbfbe4a0a4c0751b3
Akkeoss <akkeoss@laposte.net>
2023-07-05 12:37:46 +0200

    feat(core): add libretro scummvm

--- package/libretro-scummvm/libretro-scummvm.mk
+++ package/libretro-scummvm/libretro-scummvm.mk
@@ -1,36 +1,39 @@
 ################################################################################
 #
-# SCUMMVM
+# LIBRETRO-SCUMMVM
 #
 ################################################################################
 
-LIBRETRO_SCUMMVM_VERSION = 440d5d37ed8b4e0f7d0e312b74cae1bf3506a8fd
+# Release version 2.7.0 - Commit version of 06/22/2023
+LIBRETRO_SCUMMVM_VERSION = acd638cb4383e9ccc54096ffd214a0032b9bb0a6
 LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
-LIBRETRO_SCUMMVM_LICENSE = GPL-2.0
+LIBRETRO_SCUMMVM_LICENSE = GPL2
 LIBRETRO_SCUMMVM_LICENSE_FILES = COPYING
 
-ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_X86_64)$(BR2_PACKAGE_RECALBOX_TARGET_X86),y)
-LIBRETRO_SCUMMVM_LTO_CFLAGS = $(COMPILER_COMMONS_CFLAGS_NOLTO)
-LIBRETRO_SCUMMVM_LTO_CXXFLAGS = $(COMPILER_COMMONS_CXXFLAGS_NOLTO)
-LIBRETRO_SCUMMVM_LTO_LDFLAGS = $(COMPILER_COMMONS_LSFLAGS_NOLTO)
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_ODROIDXU4),y)
+LIBRETRO_SCUMMVM_PLATFORM=armv cortexa9 neon hardfloat
+else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_ODROIDGO2),y)
+LIBRETRO_SCUMMVM_PLATFORM=odroidgo2
+else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RG353X),y)
+LIBRETRO_SCUMMVM_PLATFORM=rg353x
 else
-LIBRETRO_SCUMMVM_LTO_CFLAGS = $(COMPILER_COMMONS_CFLAGS_SO)
-LIBRETRO_SCUMMVM_LTO_CXXFLAGS = $(COMPILER_COMMONS_CXXFLAGS_SO)
-LIBRETRO_SCUMMVM_LTO_LDFLAGS = $(COMPILER_COMMONS_LSFLAGS_SO)
+LIBRETRO_SCUMMVM_PLATFORM = $(RETROARCH_LIBRETRO_PLATFORM)
+endif
+
+ifeq ($(BR2_ARCH_IS_64),y)
+LIBRETRO_SCUMMVM_OPTS += BUILD_64BIT=1
 endif
 
 define LIBRETRO_SCUMMVM_BUILD_CMDS
-	$(SED) "s|-O2|-O3|g" $(@D)/backends/platform/libretro/build/Makefile
-	CFLAGS="$(TARGET_CFLAGS) $(LIBRETRO_SCUMMVM_LTO_CFLAGS) -ffat-lto-objects" \
-		CXXFLAGS="$(TARGET_CXXFLAGS) $(LIBRETRO_SCUMMVM_LTO_CXXFLAGS) -ffat-lto-objects" \
-		LDFLAGS="$(TARGET_LDFLAGS) $(LIBRETRO_SCUMMVM_LTO_LDFLAGS) -ffat-lto-objects -shared -Wl,--no-undefined" \
-		$(MAKE) TOOLSET="$(TARGET_CROSS)" -C $(@D)/backends/platform/libretro/build platform="$(RETROARCH_LIBRETRO_PLATFORM)"
+	$(SED) "s|-O2|-O3|g" $(@D)/backends/platform/libretro/Makefile
+	CFLAGS="$(TARGET_CFLAGS) $(LIBRETRO_SCUMMVM_CFLAGS_SO)" \
+		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
+		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/backends/platform/libretro -f Makefile platform="$(LIBRETRO_SCUMMVM_PLATFORM)" $(LIBRETRO_SCUMMVM_OPTS)
 endef
 
 define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/libretro-scummvm
-	unzip -o $(@D)/backends/platform/libretro/aux-data/scummvm.zip -d $(TARGET_DIR)/usr/share/libretro-scummvm
-	$(INSTALL) -D $(@D)/backends/platform/libretro/build/scummvm_libretro.so \
+	$(INSTALL) -D $(@D)/backends/platform/libretro/scummvm_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/scummvm_libretro.so
 endef
 

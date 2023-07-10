a51919668506e68cbfc1d648685020d41df19f4e
Pit64 <yrigaud@icloud.com>
2023-07-10 12:00:57 +0200

    fix(general): add missing smbus support for argon one case

--- projects/recalbox-hardware/case/installers/argonone/assets/argononed-poweroff.py
+++ projects/recalbox-hardware/case/installers/argonone/assets/argononed-poweroff.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import sys
 import os
-import smbus
+from smbus import SMBus
 import RPi.GPIO as GPIO
 rev = GPIO.RPI_REVISION
 
 if rev == 2 or rev == 3:
-    bus = smbus.SMBus(1)
+    bus = SMBus(1)
 else:
-    bus = smbus.SMBus(0)
+    bus = SMBus(0)
 
 if len(sys.argv)>1:
     bus.write_byte(0x1a,0)

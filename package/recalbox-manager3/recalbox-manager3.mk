0ed53cc5c0344aceb5922242f9d542f306c4807b
David Barbion <davidb@230ruedubac.fr>
2023-07-13 08:50:15 +0200

    chore: use local buildroot packaging

--- package/recalbox-manager3/recalbox-manager3.mk
+++ package/recalbox-manager3/recalbox-manager3.mk
@@ -4,22 +4,18 @@
 #
 ################################################################################
 
+RECALBOX_MANAGER3_VERSION = custom
+RECALBOX_MANAGER3_SITE = $(TOPDIR)/../projects/manager3
+RECALBOX_MANAGER3_SITE_METHOD = local
+RECALBOX_MANAGER3_DEPENDENCIES = host-nodejs host-nodejs-quasar
 RECALBOX_MANAGER3_LICENSE = MIT
-
-ifneq ($(GITLAB_TOKEN_WEBMANAGER3),)
-RECALBOX_MANAGER3_VERSION = 53a14a166c6261fec8134186fccf15c8fa7099a2
-RECALBOX_MANAGER3_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_WEBMANAGER3)@gitlab.com/recalbox/recalbox-manager-v3.git
-else
-RECALBOX_MANAGER3_VERSION = b1f66e32d41a84a9e05786b4c0b5ee4bc9783922
-RECALBOX_MANAGER3_SITE = https://gitlab.com/Bkg2k/recalbox-manager-v3-empty.git
-endif
-RECALBOX_MANAGER3_SITE_METHOD = git
 
 define RECALBOX_MANAGER3_BUILD_CMDS
 	if [ -f $(@D)/package-lock.json ] ; then rm $(@D)/package-lock.json ; fi
-	cd $(@D) && ls && \
-		HOME=/tmp yarn install && \
-		HOME=/tmp quasar build
+	cd $(@D) && \
+		$(NPM) install
+	cd $(@D) && \
+		$(QUASAR) build
 endef
 
 define RECALBOX_MANAGER3_INSTALL_TARGET_CMDS

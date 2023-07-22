469044461f0eab4aa2fc8bb358d018b39004cfca
Bkg2k <bkg2k9@gmail.com>
2023-07-22 13:29:20 +0200

    fix(frontend): fix crash in bios manager

--- projects/frontend/es-app/src/guis/GuiBiosScan.cpp
+++ projects/frontend/es-app/src/guis/GuiBiosScan.cpp
@@ -386,7 +386,7 @@ void GuiBiosScan::UpdateBiosList()
   for(int i = 0; i < biosManager.SystemCount(); ++i)
   {
     // Fill bios list
-    BiosList biosList = biosManager.SystemBios(i);
+    const BiosList& biosList = biosManager.SystemBios(i);
 
     // Filtered?
     if (!mShowAllSystems)

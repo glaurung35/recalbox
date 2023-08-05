947758b0714ac1f3771f7b2a040da8c4ab5db850
Pit64 <yrigaud@icloud.com>
2023-08-06 00:03:24 +0200

    feat(cores): bump lr-pcsx_rearmed

--- package/libretro-pcsx_rearmed/libretro-pcsx_rearmed.mk
+++ package/libretro-pcsx_rearmed/libretro-pcsx_rearmed.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Commit version of 2022/11/14
-LIBRETRO_PCSX_REARMED_VERSION = a4e249a1373cf6269e1e4e0d60105e72210e67d3
+# Commit version of 2023/08/24
+LIBRETRO_PCSX_REARMED_VERSION = eddc65d58065ba40ca94b22696d695a18e7a51ba
 LIBRETRO_PCSX_REARMED_SITE = $(call github,libretro,pcsx_rearmed,$(LIBRETRO_PCSX_REARMED_VERSION))
 LIBRETRO_PCSX_REARMED_LICENSE = GPL-2.0
 LIBRETRO_PCSX_REARMED_LICENSE_FILES = COPYING

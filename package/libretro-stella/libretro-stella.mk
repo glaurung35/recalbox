96d1186614a5fbefa24d873155fc1161ddc764c1
Pit64 <yrigaud@icloud.com>
2023-08-05 22:36:34 +0200

    feat(cores): bump lr-stella

--- package/libretro-stella/libretro-stella.mk
+++ package/libretro-stella/libretro-stella.mk
@@ -4,21 +4,22 @@
 #
 ################################################################################
 
-LIBRETRO_STELLA_VERSION = 84b3f0280e7a4b443de4908695a9aa3307868e20
+# Commit of 2023/09/12
+LIBRETRO_STELLA_VERSION = e030c8dea948f289954c7978605836154615bcab
 LIBRETRO_STELLA_SITE = $(call github,stella-emu,stella,$(LIBRETRO_STELLA_VERSION))
 LIBRETRO_STELLA_LICENSE = GPL-2.0
 
 define LIBRETRO_STELLA_BUILD_CMDS
-	$(SED) "s|-O2|-O3|g" $(@D)/src/libretro/Makefile
+	$(SED) "s|-O2|-O3|g" $(@D)/src/os/libretro/Makefile
 	CFLAGS="$(TARGET_CFLAGS) $(COMPILER_COMMONS_CFLAGS_SO)" \
 		CXXFLAGS="$(TARGET_CXXFLAGS) $(COMPILER_COMMONS_CXXFLAGS_SO)" \
 		LDFLAGS="$(TARGET_LDFLAGS) $(COMPILER_COMMONS_LDFLAGS_SO)" \
-		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/libretro -f \
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/src/os/libretro -f \
 		Makefile platform="$(RETROARCH_LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_STELLA_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/src/libretro/stella_libretro.so \
+	$(INSTALL) -D $(@D)/src/os/libretro/stella_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/stella_libretro.so
 endef
 

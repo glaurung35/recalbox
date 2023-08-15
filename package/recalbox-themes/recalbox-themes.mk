136f565f1176b3738dab4e32469a68a7e7070abf
Bkg2k <bkg2k9@gmail.com>
2023-08-15 14:37:56 +0200

    feat(emulator): add creativision theme

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = c89e685dcd884f311506f1fe6a7b8522021c8080
+RECALBOX_THEMES_VERSION = 34c3920d06b0bb60d1a4189ef1149e750387a636
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
 RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae

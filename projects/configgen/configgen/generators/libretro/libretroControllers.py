c440d0694021435d1ce1ebf67a88dc29fa1006e4
David Barbion <davidb@230ruedubac.fr>
2023-08-27 00:11:56 +0200

    fix: handle inverted axis, like rg351v

--- projects/configgen/configgen/generators/libretro/libretroControllers.py
+++ projects/configgen/configgen/generators/libretro/libretroControllers.py
@@ -267,8 +267,11 @@ class LibretroControllers:
             jsvalue = self.retroarchjoysticks[jskey] if not system.RotateControls else self.retroarchjoysticksTate[jskey]
             if controller.HasInput(jskey):
                 inp = controller.Input(jskey)
-                settings.setString("input_player%s_%s_minus_axis" % (controller.PlayerIndex, jsvalue), self.getJoystickSignRotated(system.RotateControls, '-', jskey) + str(inp.Id))
-                settings.setString("input_player%s_%s_plus_axis" % (controller.PlayerIndex, jsvalue),  self.getJoystickSignRotated(system.RotateControls, '+', jskey) + str(inp.Id))
+                # handle inverted axis
+                minus = "-" if inp.Value < 0 else "+"
+                plus = "+" if inp.Value < 0 else "-"
+                settings.setString("input_player%s_%s_minus_axis" % (controller.PlayerIndex, jsvalue), self.getJoystickSignRotated(system.RotateControls, minus, jskey) + str(inp.Id))
+                settings.setString("input_player%s_%s_plus_axis" % (controller.PlayerIndex, jsvalue),  self.getJoystickSignRotated(system.RotateControls, plus, jskey) + str(inp.Id))
 
         if controller.PlayerIndex == 1:
             specialMap: Dict[int, str] = {}

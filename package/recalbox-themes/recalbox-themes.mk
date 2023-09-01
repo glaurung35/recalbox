b694a03b19cd2d7524e0c6a2ffeee1f670ccd0d4
Pit64 <yrigaud@icloud.com>
2023-09-01 19:36:37 +0200

    feat(emulator): update theme

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -8,7 +8,7 @@ ifneq ($(GITLAB_TOKEN_THEMES),)
 RECALBOX_THEMES_VERSION = bbfb216a4a1995cbd17eb15e3bfec1dcb5c69acf
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
-RECALBOX_THEMES_VERSION = dd10b43c5bf48861274a77af9e604b1fa5bcc2ae
+RECALBOX_THEMES_VERSION = c89e685dcd884f311506f1fe6a7b8522021c8080
 RECALBOX_THEMES_SITE = https://gitlab.com/recalbox/recalbox-themes
 endif
 RECALBOX_THEMES_SITE_METHOD = git

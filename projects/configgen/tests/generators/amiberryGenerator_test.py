28e5875800be83649edac5656546844026f3a0c7
David Barbion <davidb@230ruedubac.fr>
2023-09-02 13:57:36 +0200

    chore: make controller configuration as more generic and real life

--- projects/configgen/tests/generators/amiberryGenerator_test.py
+++ projects/configgen/tests/generators/amiberryGenerator_test.py
@@ -30,23 +30,6 @@ def emulator(mocker, fake_process):
     return AmiberryGenerator()
 
 
-@pytest.fixture
-def controller_configuration():
-    uuid = "060000004c0500006802000000010000"
-    return controllersConfig.Controller.LoadUserControllerConfigurations(
-        p1index =-1, p1guid =uuid, p1name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p1devicepath ="", p1nbaxes = -1, p1nbhats = -1, p1nbbuttons = -1,
-        p2index =-1, p2guid =uuid, p2name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p2devicepath ="", p2nbaxes = -1, p2nbhats = -1, p2nbbuttons = -1,
-        p3index =-1, p3guid =uuid, p3name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p3devicepath ="", p3nbaxes = -1, p3nbhats = -1, p3nbbuttons = -1,
-        p4index =-1, p4guid =uuid, p4name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p4devicepath ="", p4nbaxes = -1, p4nbhats = -1, p4nbbuttons = -1,
-        p5index =-1, p5guid =uuid, p5name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p5devicepath ="", p5nbaxes = -1, p5nbhats = -1, p5nbbuttons = -1,
-        p6index =-1, p6guid =uuid, p6name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p6devicepath ="", p6nbaxes = -1, p6nbhats = -1, p6nbbuttons = -1,
-        p7index =-1, p7guid =uuid, p7name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p7devicepath ="", p7nbaxes = -1, p7nbhats = -1, p7nbbuttons = -1,
-        p8index =-1, p8guid =uuid, p8name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p8devicepath ="", p8nbaxes = -1, p8nbhats = -1, p8nbbuttons = -1,
-        p9index =-1, p9guid =uuid, p9name ="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p9devicepath ="", p9nbaxes = -1, p9nbhats = -1, p9nbbuttons = -1,
-        p10index=-1, p10guid=uuid, p10name="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)", p10devicepath="", p10nbaxes= -1, p10nbhats= -1, p80nbbuttons= -1
-    )
-
-
 @pytest.fixture(params=[
     pytest.lazy_fixture('system_a600'),
     pytest.lazy_fixture('system_a1200'),
@@ -79,12 +62,14 @@ def system_cd32():
     return Emulator(name='amigacd32', videoMode='16', ratio='auto', emulator='amiberry', core='amiberry')
 
 
+@pytest.mark.usefixtures("controller_configuration")
 def test_simple_generate_rom_iso(emulator, system_hddiso, controller_configuration):
     command = emulator.generate(system_hddiso, controller_configuration, keyValueSettings("", False), Arguments('path/to/test.iso'))
     assert command.videomode == '16'
     assert command.array == ['/usr/bin/amiberry', '-config', '/tmp/amiga/conf/default.uae']
 
 
+@pytest.mark.usefixtures("controller_configuration")
 def test_simple_generate_rom_zip(emulator, system_hddiso, controller_configuration):
     command = emulator.generate(system_hddiso, controller_configuration, keyValueSettings("", False), Arguments('path/to/test.zip'))
     assert command.videomode == '16'

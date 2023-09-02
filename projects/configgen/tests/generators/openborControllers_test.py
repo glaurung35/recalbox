ec3101fbf7d3e1f987dccebcb2fb4069859b9de9
David Barbion <davidb@230ruedubac.fr>
2023-09-02 13:57:36 +0200

    chore: make controller configuration as more generic and real life

--- projects/configgen/tests/generators/openborControllers_test.py
+++ projects/configgen/tests/generators/openborControllers_test.py
@@ -22,35 +22,23 @@ def openbor_controller_init(self):
 
 
 @pytest.fixture
-def controller_configuration():
-    return controllersConfig.Controller.LoadUserControllerConfigurations(
-        p1index=0,
-        p1guid="060000004c0500006802000000010000",
-        p1name="PLAYSTATION(R)3 Controller (00:48:E8:D1:63:25)",
-        p1devicepath="",
-        p1nbaxes=-1,
-        p1nbhats=-1,
-        p1nbbuttons=-1,
-    )
-
-
-@pytest.fixture
 def settings():
     return keyValueSettings("/tmp/dumb")
 
 
+@pytest.mark.usefixtures("controller_configuration")
 def test_openborControllers(controller_configuration, settings):
     controllers = OpenborControllers(controller_configuration)
     controllers.addControllers(settings)
     assert (
-        settings.getInt("JoystickButton-P0-MOVEDOWN", "undefined-not-good") == 607
-        and settings.getInt("JoystickButton-P0-MOVEUP", "undefined-not-good") == 605
-        and settings.getInt("JoystickButton-P0-MOVELEFT", "undefined-not-good") == 608
-        and settings.getInt("JoystickButton-P0-MOVERIGHT", "undefined-not-good") == 606
-        and settings.getInt("JoystickButton-P0-ATTACK", "undefined-not-good") == 614
-        and settings.getInt("JoystickButton-P0-ATTACK2", "undefined-not-good") == 615
-        and settings.getInt("JoystickButton-P0-ATTACK3", "undefined-not-good") == 613
-        and settings.getInt("JoystickButton-P0-ATTACK4", "undefined-not-good") == 616
-        and settings.getInt("JoystickButton-P0-HOTKEY", "undefined-not-good") == 617
-        and settings.getInt("JoystickButton-P0-JUMP", "undefined-not-good") == 611
+        settings.getInt("JoystickButton-P0-MOVEDOWN", "undefined-not-good") == 615
+        and settings.getInt("JoystickButton-P0-MOVEUP", "undefined-not-good") == 614
+        and settings.getInt("JoystickButton-P0-MOVELEFT", "undefined-not-good") == 616
+        and settings.getInt("JoystickButton-P0-MOVERIGHT", "undefined-not-good") == 617
+        and settings.getInt("JoystickButton-P0-ATTACK", "undefined-not-good") == 602
+        and settings.getInt("JoystickButton-P0-ATTACK2", "undefined-not-good") == 601
+        and settings.getInt("JoystickButton-P0-ATTACK3", "undefined-not-good") == 603
+        and settings.getInt("JoystickButton-P0-ATTACK4", "undefined-not-good") == 604
+        and settings.getInt("JoystickButton-P0-HOTKEY", "undefined-not-good") == 611
+        and settings.getInt("JoystickButton-P0-JUMP", "undefined-not-good") == 605
     )

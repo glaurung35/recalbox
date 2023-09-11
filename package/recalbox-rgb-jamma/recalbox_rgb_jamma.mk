c0f88af4cb23f5dd9217ba05da446839c69e4665
digitalLumberjack <digitallumberjack@gmail.com>
2023-09-11 17:56:27 +0200

    feat(system): rrgbj: add autodetection of v3

--- package/recalbox-rgb-jamma/recalbox_rgb_jamma.mk
+++ package/recalbox-rgb-jamma/recalbox_rgb_jamma.mk
@@ -12,12 +12,12 @@ RECALBOX_RGB_JAMMA_DEPENDENCIES = linux recalbox-rgb-dual
 
 define RECALBOX_RGB_JAMMA_BUILD_CMDS
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-overlay.dts -o $(@D)/recalboxrgbjamma.dtbo
-	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjammapoll-overlay.dts -o $(@D)/recalboxrgbjammapoll.dtbo
+	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-v2-overlay.dts -o $(@D)/recalboxrgbjamma-v2.dtbo
 endef
 
 define RECALBOX_RGB_JAMMA_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjammapoll.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjammapoll.dtbo
+	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma-v2.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma-v2.dtbo
 	$(INSTALL) -D -m 0744 $(RECALBOX_RGB_JAMMA_PKGDIR)/S03rrgbjamma $(TARGET_DIR)/etc/init.d/
 endef
 

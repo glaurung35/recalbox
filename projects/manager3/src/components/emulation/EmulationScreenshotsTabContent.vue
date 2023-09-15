07fdaeb057121de4d2fc139b8cd2f6436b72167b
Pit64 <yrigaud@icloud.com>
2023-09-16 00:52:22 +0200

    feat(manager): add video support in screenshots

--- projects/manager3/src/components/emulation/EmulationScreenshotsTabContent.vue
+++ projects/manager3/src/components/emulation/EmulationScreenshotsTabContent.vue
@@ -8,35 +8,76 @@
       class="col col-xs-12 col-sm-4 col-md-3 q-mb-md q-pl-sm q-pr-sm"
       v-for="screenshot in screenshots"
     >
-      <q-card @click="openScreenshot(screenshot.url)" class="screenshot" flat rounded>
-        <q-card-section horizontal>
-          <q-img :src="screenshot.url" class="col" loading="lazy">
-            <div
-              class="absolute-bottom text-white justify-between row items-start"
-              style="padding: .5em"
-            >
+      <div v-if="screenshot.urlImage">
+        <q-card @click="openScreenshot(screenshot.urlImage)" class="screenshot" flat rounded>
+          <q-card-section horizontal>
+            <q-img :src="screenshot.urlImage" class="col" loading="lazy">
+              <div
+                class="absolute-bottom text-white justify-between row items-start"
+                style="padding: .5em"
+              >
+                <span class="self-center"><q-icon
+                  name="mdi-calendar-clock"
+                  style="font-size: 1.5em; margin-right: .25em;"
+                />
+                {{ screenshot.date }}</span>
+                <q-btn
+                  class="float-right"
+                  color="negative"
+                  flat icon="mdi-delete"
+                  round
+                  @click.stop="openDeleteConfirm(screenshot.name)"
+                />
+              </div>
+            </q-img>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div v-if="screenshot.urlVideo">
+        <q-card @click="openVideo(screenshot.urlVideo)" class="screenshot" flat rounded>
+          <q-card-section horizontal>
+            <q-img src="../../assets/video-poster.jpg" class="col" loading="lazy">
+              <div
+                class="absolute-bottom text-white justify-between row items-start"
+                style="padding: .5em"
+              >
               <span class="self-center"><q-icon
                 name="mdi-calendar-clock"
                 style="font-size: 1.5em; margin-right: .25em;"
               />
               {{ screenshot.date }}</span>
-              <q-btn
-                class="float-right"
-                color="negative"
-                flat icon="mdi-delete"
-                round
-                @click.stop="openDeleteConfirm(screenshot.name)"
-              />
-            </div>
-          </q-img>
-        </q-card-section>
-      </q-card>
+                <q-btn
+                  class="float-right"
+                  color="negative"
+                  flat icon="mdi-delete"
+                  round
+                  @click.stop="openDeleteVideoConfirm(screenshot.name)"
+                />
+              </div>
+            </q-img>
+          </q-card-section>
+        </q-card>
+      </div>
 
     </div>
-    <q-dialog v-model="modal.open" full-height full-width>
+    <q-dialog v-model="modalImage.open" full-height full-width>
       <q-card>
         <q-card-section horizontal>
-          <q-img :src="modal.imgUrl" @click="modal.open = false" class="opened"/>
+          <q-img :src="modalImage.imgUrl" @click="modalImage.open = false" class="opened"/>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="modalVideo.open" full-height full-width>
+      <q-card>
+        <q-card-section horizontal>
+          <q-media-player
+            type="video"
+            :sources="[{ src: modalVideo.videoUrl, type: 'video/mp4' }]"
+            style="height: 1000px; margin: 0 auto"
+            @click="modalVideo.open = false"
+            class="opened"
+          />
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -59,18 +100,18 @@ mediaStore.fetch();
 
 const { screenshots } = storeToRefs(mediaStore);
 
-const modal = ref<object>({
+const modalImage = ref<object>({
   open: false,
   imgUrl: '',
   selectedName: null,
 });
 
 function openScreenshot(url:string) {
-  modal.value.imgUrl = url;
-  modal.value.open = true;
+  modalImage.value.imgUrl = url;
+  modalImage.value.open = true;
 }
 function openDeleteConfirm(name:string) {
-  modal.value.selectedName = name;
+  modalImage.value.selectedName = name;
   $q.dialog({
     class: 'delete-dialog-card',
     dark: true,
@@ -80,8 +121,35 @@ function openDeleteConfirm(name:string) {
     transitionHide: 'flip-up',
     transitionShow: 'flip-down',
   }).onOk(() => {
-    mediaStore.delete(modal.value.selectedName);
-    modal.value.selectedName = null;
+    mediaStore.delete(modalImage.value.selectedName);
+    modalImage.value.selectedName = null;
+  });
+}
+
+const modalVideo = ref<object>({
+  open: false,
+  videoUrl: '',
+  selectedName: null,
+});
+
+function openVideo(url:string) {
+  modalVideo.value.videoUrl = url;
+  modalVideo.value.open = true;
+}
+
+function openDeleteVideoConfirm(name:string) {
+  modalImage.value.selectedName = name;
+  $q.dialog({
+    class: 'delete-dialog-card',
+    dark: true,
+    message: t('emulation.screenshots.dialogs.removevideo.text'),
+    cancel: true,
+    persistent: true,
+    transitionHide: 'flip-up',
+    transitionShow: 'flip-down',
+  }).onOk(() => {
+    mediaStore.delete(modalImage.value.selectedName);
+    modalImage.value.selectedName = null;
   });
 }
 </script>

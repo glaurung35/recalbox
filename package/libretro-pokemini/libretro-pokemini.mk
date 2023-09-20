37b5944524cb6700ff4d32cc2e2130a262ee05ff
Pit64 <yrigaud@icloud.com>
2023-09-20 15:06:09 +0200

    feat(cores): bump lr-pokemini

--- package/libretro-pokemini/libretro-pokemini.mk
+++ package/libretro-pokemini/libretro-pokemini.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-LIBRETRO_POKEMINI_VERSION = d2ea2ef52f97ea6f7bfff57b5367d95083474414
+# Commit of 2023/05/28
+LIBRETRO_POKEMINI_VERSION = 9bf450887026d9b92d4f9432b5d2a5ed749a35e2
 LIBRETRO_POKEMINI_SITE = $(call github,libretro,PokeMini,$(LIBRETRO_POKEMINI_VERSION))
 LIBRETRO_POKEMINI_LICENSE = GPL-3.0+
 LIBRETRO_POKEMINI_LICENSE_FILES = LICENSE
@@ -20,6 +21,7 @@ endef
 define LIBRETRO_POKEMINI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/pokemini_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/pokemini_libretro.so
+	mkdir -p $(TARGET_DIR)/recalbox/share_upgrade/bios/pokemini
 endef
 
 $(eval $(generic-package))

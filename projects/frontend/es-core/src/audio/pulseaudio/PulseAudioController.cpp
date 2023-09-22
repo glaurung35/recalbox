3e22b24a2fa50751b40299c0594ffb178684a31f
Bkg2k <bkg2k9@gmail.com>
2023-09-22 09:53:00 +0200

    fix(frontend): fix warnings

--- projects/frontend/es-core/src/audio/pulseaudio/PulseAudioController.cpp
+++ projects/frontend/es-core/src/audio/pulseaudio/PulseAudioController.cpp
@@ -1289,6 +1289,7 @@ String PulseAudioController::GetPortDescription(const pa_card_port_info& info, A
     case BoardType::RG353V:
     case BoardType::RG353M:
     case BoardType::RG503:
+    case BoardType::RG351V:
     case BoardType::PCx86:
     case BoardType::PCx64:
     case BoardType::UndetectedYet:

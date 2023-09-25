49ad810838db33c6792feb8eedf03d3958edec0c
digitalLumberjack <digitallumberjack@gmail.com>
2023-09-25 15:53:18 +0200

    feat(system): adapt gpicase dtbo to kernel 6.1

--- package/dpi-pizero-gpicase/dpi-pizero-gpicase.mk
+++ package/dpi-pizero-gpicase/dpi-pizero-gpicase.mk
@@ -9,11 +9,14 @@ DPI_PIZERO_GPICASE_SITE = $(call github,ian57,dpi-pizero-gpicase,$(DPI_PIZERO_GP
 DPI_PIZERO_GPICASE_LICENSE = GPL-2.0
 DPI_PIZERO_GPICASE_LICENSE_FILES = LICENSE
 
-DPI_PIZERO_GPICASE_DEPENDENCIES += rpi-firmware
+DPI_PIZERO_GPICASE_DEPENDENCIES += linux
+
+define DPI_PIZERO_GPICASE_BUILD_CMDS
+	$(HOST_DIR)/bin/linux-dtc $(@D)/dpi-pizero-gpicase.dts -o $(@D)/dpi-pizero-gpicase.dtbo
+endef
 
 define DPI_PIZERO_GPICASE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/dpi-pizero-gpicase.dtbo $(BINARIES_DIR)/boot-data/overlays/dpi-pizero-gpicase.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/dpi-pizero-gpicase.dts $(BINARIES_DIR)/boot-data/overlays/dpi-pizero-gpicase.dts
 endef
 
 $(eval $(generic-package))

f62adc4ba3ac64c22ce759156c46631eb290fc35
digitalLumberjack <digitallumberjack@gmail.com>
2023-09-25 15:53:18 +0200

    feat(system): adapt gpicase dtbo to kernel 6.1

--- package/pwm-audio-pizero-gpicase/pwm-audio-pizero-gpicase.mk
+++ package/pwm-audio-pizero-gpicase/pwm-audio-pizero-gpicase.mk
@@ -4,16 +4,19 @@
 #
 ################################################################################
 
-PWM_AUDIO_PIZERO_GPICASE_VERSION = 4fbcbce5643d243784bd71d8215ac58476c8dacd
-PWM_AUDIO_PIZERO_GPICASE_SITE = $(call github,ian57,pwm-audio-pizero-gpicase,$(PWM_AUDIO_PIZERO_GPICASE_VERSION))
+PWM_AUDIO_PIZERO_GPICASE_VERSION = 2e7aed359fcc537cf085f021242f5c9e692d8aaf
+PWM_AUDIO_PIZERO_GPICASE_SITE = $(call github,digitalLumberjack,pwm-audio-pizero-gpicase,$(PWM_AUDIO_PIZERO_GPICASE_VERSION))
 PWM_AUDIO_PIZERO_GPICASE_LICENSE = GPL-2.0
 PWM_AUDIO_PIZERO_GPICASE_LICENSE_FILES = LICENSE
 
-PWM_AUDIO_PIZERO_GPICASE_DEPENDENCIES += rpi-firmware
+PWM_AUDIO_PIZERO_GPICASE_DEPENDENCIES += linux
+
+define PWM_AUDIO_PIZERO_GPICASE_BUILD_CMDS
+	$(HOST_DIR)/bin/linux-dtc $(@D)/pwm-audio-pizero-gpicase.dts -o $(@D)/pwm-audio-pizero-gpicase.dtbo
+endef
 
 define PWM_AUDIO_PIZERO_GPICASE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pizero-gpicase.dtbo $(BINARIES_DIR)/boot-data/overlays/pwm-audio-pizero-gpicase.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/pwm-audio-pizero-gpicase.dts $(BINARIES_DIR)/boot-data/overlays/pwm-audio-pizero-gpicase.dts
 endef
 
 $(eval $(generic-package))

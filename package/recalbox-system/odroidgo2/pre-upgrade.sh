d333b324a9e4fe5719fe29b1302138112928b83d
Pit64 <yrigaud@icloud.com>
2023-09-28 01:17:58 +0200

    feat(dats): remove all old dats

--- package/recalbox-system/odroidgo2/pre-upgrade.sh
+++ package/recalbox-system/odroidgo2/pre-upgrade.sh
@@ -26,10 +26,22 @@ if [ "$OLDBOOT" = 'odroidgoa-uboot-config' ]; then
   sed -i -E '1 s/^.*$/'"$OLDBOOT"'/' /boot/update/boot.ini
 fi
 
-
 # Remove old dats files from bios
+rm -rf /recalbox/share/bios/fba_029671*.dat
+rm -rf /recalbox/share/bios/fba/*.dat
+rm -rf /recalbox/share/bios/FinalBurn*.dat
 rm -rf /recalbox/share/bios/dc/*.dat.zip
+rm -rf /recalbox/share/bios/dc/*barhi.zip
+rm -rf /recalbox/share/bios/fbneo/FinalBurn*.dat
+rm -rf /recalbox/share/bios/fbneo/light
 rm -rf /recalbox/share/bios/mame/*.dat.zip
+rm -rf /recalbox/share/bios/mame/*rhi.zip
+rm -rf /recalbox/share/bios/mame2000/MAME\ 0.37b5\ XML.dat
+rm -rf /recalbox/share/bios/mame2003/mame2003.xml
+rm -rf /recalbox/share/bios/mame2003-plus/mame2003-plus.xml
+rm -rf /recalbox/share/bios/mame2010/mame2010.xml
+rm -rf /recalbox/share/bios/mame2015/mame2014-xml.zip
+rm -rf /recalbox/share/bios/mame2015/mame2015-xml.zip
 
 # Remove all .nvmem and .nvmem2 files from saves
 rm -rf /recalbox/share/saves/atomiswave/reicast/*.nvmem

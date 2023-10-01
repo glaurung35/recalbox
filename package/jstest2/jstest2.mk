2f8f74972ce633cb6afbd6a5c8f215dd8122faba
Pit64 <yrigaud@icloud.com>
2023-10-01 10:44:24 +0200

    fix(system): change sdl-jstest source
    Original source has been deleted.

--- package/jstest2/jstest2.mk
+++ package/jstest2/jstest2.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 JSTEST2_VERSION = aafbdb1ed3e687583037ba55ae88b1210d6ce98b
-JSTEST2_SITE = https://gitlab.com/sdl-jstest/sdl-jstest.git
+JSTEST2_SITE = https://github.com/Grumbel/sdl-jstest.git
 JSTEST2_SITE_METHOD = git
 JSTEST2_GIT_SUBMODULES = y
 JSTEST2_DEPENDENCIES = sdl sdl2

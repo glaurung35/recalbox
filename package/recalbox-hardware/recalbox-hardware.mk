15d9958932e38b92b132d7e9486824ce0c1ab1d2
David Barbion <davidb@230ruedubac.fr>
2023-10-02 22:11:31 +0200

    chore: install gpicase dtbo in INSTALL_IMAGES steps
    For that purpose and to improve readability, a new virtual target is created:
    BR2_PACKAGE_RECALBOX_TARGET_RPI_ANY
    which is set to y for any RPI

--- package/recalbox-hardware/recalbox-hardware.mk
+++ package/recalbox-hardware/recalbox-hardware.mk
@@ -10,21 +10,24 @@ RECALBOX_HARDWARE_SITE_METHOD = local
 RECALBOX_HARDWARE_LICENSE = MIT
 RECALBOX_HARDWARE_LICENSE_FILES = LICENSE.md
 RECALBOX_HARDWARE_DEPENDENCIES = recalbox-themes host-python3 linux
+RECALBOX_HARDWARE_INSTALL_IMAGES = YES
 
-
-define RECALBOX_HARDWARE_BUILD_PI
+define RECALBOX_HARDWARE_BUILD_CMDS_PI
 	$(HOST_DIR)/bin/linux-dtc $(@D)/case/installers/retroflags/assets/overlays/retroflag-case-overlay.dts -o $(@D)/retroflag-case.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/retroflag-case.dtbo $(BINARIES_DIR)/boot-data/overlays/retroflag-case.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/case/installers/gpi/assets/overlays/recalbox-gpicase1-overlay.dts -o $(@D)/recalbox-gpicase1.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/recalbox-gpicase1.dtbo $(BINARIES_DIR)/boot-data/overlays/recalbox-gpicase1.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/case/installers/gpi2w/assets/overlays/recalbox-gpicase2w-overlay.dts -o $(@D)/recalbox-gpicase2w.dtbo
+endef
+
+define RECALBOX_HARDWARE_INSTALL_IMAGE_CMDS_PI
+	$(INSTALL) -D -m 0644 $(@D)/retroflag-case.dtbo $(BINARIES_DIR)/boot-data/overlays/retroflag-case.dtbo
+	$(INSTALL) -D -m 0644 $(@D)/recalbox-gpicase1.dtbo $(BINARIES_DIR)/boot-data/overlays/recalbox-gpicase1.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalbox-gpicase2w.dtbo $(BINARIES_DIR)/boot-data/overlays/recalbox-gpicase2w.dtbo
 endef
 
-ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI3)$(BR2_PACKAGE_RECALBOX_TARGET_RPI4)$(BR2_PACKAGE_RECALBOX_TARGET_RPI4_64),y)
-RECALBOX_HARDWARE_POST_BUILD_HOOKS += RECALBOX_HARDWARE_BUILD_PI
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI_ANY),y)
+RECALBOX_HARDWARE_POST_BUILD_HOOKS += RECALBOX_HARDWARE_BUILD_CMDS_PI
+RECALBOX_HARDWARE_POST_INSTALL_IMAGES_HOOKS += RECALBOX_HARDWARE_INSTALL_IMAGE_CMDS_PI
 endif
-
 
 define RECALBOX_HARDWARE_BUILD_CMDS
 	PYTHONPATH="$(PYTHON3_PATH)" $(HOST_DIR)/bin/python$(PYTHON3_VERSION_MAJOR) -m compileall $(@D)

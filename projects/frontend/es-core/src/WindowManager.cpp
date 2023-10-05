3cbc0466eae6761017ad63eec3a2697e520ad08e
Bkg2k <bkg2k9@gmail.com>
2023-10-05 19:32:30 +0200

    fix(frontend): fix osd positions & size in low resolutions

--- projects/frontend/es-core/src/WindowManager.cpp
+++ projects/frontend/es-core/src/WindowManager.cpp
@@ -278,8 +278,6 @@ void WindowManager::Render(Transform4x4f& transform)
 
   // Reset matrix
   Renderer::SetMatrix(Transform4x4f::Identity());
-  // Display host machine battery state if available
-  DisplayBatteryState();
   // Then popups
   InfoPopupsDisplay(transform);
   // Pad OSD
@@ -364,39 +362,6 @@ bool WindowManager::KonamiCode(const InputCompactEvent& input)
   }
 
   return false;
-}
-
-void WindowManager::DisplayBatteryState()
-{
-  if (!Board::Instance().HasBattery() ||
-      RecalboxConf::Instance().GetBatteryHidden()) return;
-
-  int charge = Board::Instance().BatteryChargePercent();
-  UnicodeChar unicodeIcon = 0xf1b4;
-  if (!Board::Instance().IsBatteryCharging())
-  {
-    if (charge >= 66)      unicodeIcon = 0xF1ba;
-    else if (charge >= 33) unicodeIcon = 0xF1b8;
-    else if (charge >= 15) unicodeIcon = 0xF1b1;
-    else                   unicodeIcon = 0xF1b5;
-  }
-
-  //int charge = 9; UnicodeChar unicodeIcon = 0xF1b1;
-  unsigned int color = 0xFFFFFFFF;
-  if (charge < 15) color = 0xFF8000FF;
-  if ((charge < 10) && ((SDL_GetTicks() >> 8) & 3) == 0) color = 0xFF0000FF;
-
-  Font* font = Font::get(Renderer::Instance().Is240p() ? (int)FONT_SIZE_LARGE : (int)FONT_SIZE_MEDIUM).get();
-  Font::Glyph& glyph = font->Character(unicodeIcon);
-  float glyphWidth = glyph.texSize.x() * (float) glyph.texture->textureSize.x();
-  float glyphHeight = glyph.texSize.y() * (float) glyph.texture->textureSize.y();
-  float x = Renderer::Instance().DisplayWidthAsFloat() - Math::round(glyphWidth * 1.25f);
-  float y = Math::round(glyphHeight * 0.25f);
-  font->renderCharacter(unicodeIcon, x + 1, y, 1.f, 1.f, 0xFF);
-  font->renderCharacter(unicodeIcon, x - 1, y, 1.f, 1.f, 0xFF);
-  font->renderCharacter(unicodeIcon, x, y + 1, 1.f, 1.f, 0xFF);
-  font->renderCharacter(unicodeIcon, x, y - 1, 1.f, 1.f, 0xFF);
-  font->renderCharacter(unicodeIcon, x, y, 1.f, 1.f, color);
 }
 
 void WindowManager::RenderAll(bool halfLuminosity)

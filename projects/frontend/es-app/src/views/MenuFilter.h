3514c32c41bbb6f9b77aa6e0e3a7c8acab35af7f
digitalLumberjack <digitallumberjack@gmail.com>
2023-10-13 17:33:13 +0200

    fix(frontend): remove SimpleGameListView menu access when emulationstation.menu is none

---
+++ projects/frontend/es-app/src/views/MenuFilter.h
@@ -0,0 +1,17 @@
+//
+// Created by digi on 10/13/23.
+//
+
+#pragma once
+
+class MenuFilter
+{
+  public:
+    enum Menu {
+      Main,
+      Exit,
+      Search,
+      GamelistOptions
+    };
+    static bool ShouldDisplayMenu(const enum Menu menu) { return RecalboxConf::Instance().GetMenuType() !=RecalboxConf::Menu::None; }
+};

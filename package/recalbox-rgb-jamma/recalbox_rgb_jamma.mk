55624c4e0dbdb636a7bfef079983319e3aa32f47
digitalLumberjack <digitallumberjack@gmail.com>
2023-10-17 21:05:08 +0200

    feat(system): pi5: fix rrgbj v4 support

--- package/recalbox-rgb-jamma/recalbox_rgb_jamma.mk
+++ package/recalbox-rgb-jamma/recalbox_rgb_jamma.mk
@@ -12,6 +12,7 @@ RECALBOX_RGB_JAMMA_DEPENDENCIES = linux recalbox-rgb-dual
 
 define RECALBOX_RGB_JAMMA_BUILD_CMDS
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-overlay.dts -o $(@D)/recalboxrgbjamma.dtbo
+	$(HOST_DIR)/bin/linux-dtc $(@D)/i2c1-pi5-overlay.dts -o $(@D)/i2c1-pi5.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-pi5-overlay.dts -o $(@D)/recalboxrgbjamma-pi5.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-v2-overlay.dts -o $(@D)/recalboxrgbjamma-v2.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbjamma-v3-overlay.dts -o $(@D)/recalboxrgbjamma-v3.dtbo
@@ -19,6 +20,8 @@ endef
 
 define RECALBOX_RGB_JAMMA_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma.dtbo
+	# Working overlay for runtime dtoverlay loading
+	$(INSTALL) -D -m 0644 $(@D)/i2c1-pi5.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/i2c1-pi5.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma-pi5.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma-pi5.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma-v2.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma-v2.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbjamma-v3.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/recalboxrgbjamma-v3.dtbo

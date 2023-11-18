4eae748a73404421ffe9a43393347b01801993e4
digitalLumberjack <digitallumberjack@gmail.com>
2023-11-18 14:17:35 +0100

    feat(system): rrgbd: add rpi5 support for recalbox rgb dual
    - no sound support yet, of course

--- package/recalbox-rgb-dual/recalbox_rgb_dual.mk
+++ package/recalbox-rgb-dual/recalbox_rgb_dual.mk
@@ -12,10 +12,14 @@ RECALBOX_RGB_DUAL_DEPENDENCIES = linux
 
 define RECALBOX_RGB_DUAL_BUILD_CMDS
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-overlay.dts -o $(@D)/recalboxrgbdual.dtbo
-	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-pi5-overlay.dts -o $(@D)/recalboxrgbdual-pi5.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-thirdparty-overlay.dts -o $(@D)/recalboxrgbdual-thirdparty.dtbo
 	$(HOST_DIR)/bin/linux-dtc $(@D)/headphones-overlay.dts -o $(@D)/headphones.dtbo
 endef
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI5_64),y)
+# Overwrite the dtbo with the pi5 one.
+RECALBOX_RGB_DUAL_BUILD_CMDS += && $(HOST_DIR)/bin/linux-dtc $(@D)/recalboxrgbdual-pi5-overlay.dts -o $(@D)/recalboxrgbdual.dtbo
+endif
 
 define RECALBOX_RGB_DUAL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/hat_map.dtb $(BINARIES_DIR)/boot-data/overlays/hat_map.dtb

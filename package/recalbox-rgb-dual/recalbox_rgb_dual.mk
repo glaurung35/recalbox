06f1c250fc3dfc44ef583ef8c24c447da82dfb4d
digitalLumberjack <digitallumberjack@gmail.com>
2023-11-19 10:42:30 +0100

    fix: fix beta build

--- package/recalbox-rgb-dual/recalbox_rgb_dual.mk
+++ package/recalbox-rgb-dual/recalbox_rgb_dual.mk
@@ -24,7 +24,6 @@ endif
 define RECALBOX_RGB_DUAL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/hat_map.dtb $(BINARIES_DIR)/boot-data/overlays/hat_map.dtb
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbdual.dtbo $(BINARIES_DIR)/boot-data/overlays/recalboxrgbdual.dtbo
-	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbdual-pi5.dtbo $(BINARIES_DIR)/boot-data/overlays/recalboxrgbdual-pi5.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/recalboxrgbdual-thirdparty.dtbo $(BINARIES_DIR)/boot-data/overlays/recalboxrgbdual-thirdparty.dtbo
 	$(INSTALL) -D -m 0644 $(@D)/headphones.dtbo $(BINARIES_DIR)/boot-data/overlays/headphones.dtbo
 endef

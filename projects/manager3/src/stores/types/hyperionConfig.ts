97f2f7dabe2cf9125bbaa44ee98f22f45ed463e5
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 02:49:34 +0100

    feat(webmanager): remove zod for hyperion config

--- projects/manager3/src/stores/types/hyperionConfig.ts
+++ projects/manager3/src/stores/types/hyperionConfig.ts
@@ -1,13 +1,10 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
-import { z } from 'zod';
 
-export const HyperionConfigSchema = z.object({
-  enabled: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-});
-
-export type HyperionConfig = z.infer<typeof HyperionConfigSchema>
+export interface HyperionConfigResponse {
+  enabled: {
+    exist: boolean;
+    value: boolean;
+  };
+}

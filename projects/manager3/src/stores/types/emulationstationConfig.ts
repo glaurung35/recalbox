1fc82b79b18f546fa4b4f1bc1ae93a24012160c6
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 02:34:40 +0100

    feat(webmanager): remove zod for emulationstation config

--- projects/manager3/src/stores/types/emulationstationConfig.ts
+++ projects/manager3/src/stores/types/emulationstationConfig.ts
@@ -1,390 +1,385 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
-import { z } from 'zod';
 
-export const EmulationStationConfigSchema = z.object({
-  bootongamelist: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  filteradultgames: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  gamelistonly: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  forcebasicgamelistview: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  menu: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  'collection.lastplayed': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'videosnaps.loop': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  selectedsystem: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  'collection.multiplayer': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'collection.allgames': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'videosnaps.delay': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  hidesystemview: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  clock: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  favoritesonly: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  showgameclipclippingitem: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  showgamecliphelpitems: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'screensaver.time': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  'screensaver.type': z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  'theme.folder': z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  'collection.tate': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  virtualarcade: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.includeneogeo': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.hideoriginals': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'collection.ports.hide': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'collection.lightgun.hide': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  quicksystemselect: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  showhelp: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'popoup.help': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  'popoup.music': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  'popoup.netplay': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  systemsorting: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  'theme.carousel': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'theme.transition': z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  brightness: z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  showhidden: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  showonlylatestversion: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  hidenogames: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'arcade.view.enhanced': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hideclones': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hidebios': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hidenonworking': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'arcade.usedatabasenames': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.manufacturers': z.object({
-    exist: z.boolean(),
-    value: z.array(z.string()),
-  }),
-  debuglogs: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'pads.osd': z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  'pads.osd.type': z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-});
+export interface EmulationStationConfigResponse {
+  bootongamelist: {
+    exist: boolean;
+    value: boolean;
+  };
+  filteradultgames: {
+    exist: boolean;
+    value: boolean;
+  };
+  gamelistonly: {
+    exist: boolean;
+    value: boolean;
+  };
+  forcebasicgamelistview: {
+    exist: boolean;
+    value: boolean;
+  };
+  menu: {
+    exist: boolean;
+    value: string;
+  };
+  'collection.lastplayed': {
+    exist: boolean;
+    value: boolean;
+  };
+  'videosnaps.loop': {
+    exist: boolean;
+    value: number;
+  };
+  selectedsystem: {
+    exist: boolean;
+    value: string;
+  };
+  'collection.multiplayer': {
+    exist: boolean;
+    value: boolean;
+  };
+  'collection.allgames': {
+    exist: boolean;
+    value: boolean;
+  };
+  'videosnaps.delay': {
+    exist: boolean;
+    value: number;
+  };
+  hidesystemview: {
+    exist: boolean;
+    value: boolean;
+  };
+  clock: {
+    exist: boolean;
+    value: boolean;
+  };
+  favoritesonly: {
+    exist: boolean;
+    value: boolean;
+  };
+  showgameclipclippingitem: {
+    exist: boolean;
+    value: boolean;
+  };
+  showgamecliphelpitems: {
+    exist: boolean;
+    value: boolean;
+  };
+  'screensaver.time': {
+    exist: boolean;
+    value: number;
+  };
+  'screensaver.type': {
+    exist: boolean;
+    value: string;
+  };
+  'theme.folder': {
+    exist: boolean;
+    value: string;
+  };
+  'collection.tate': {
+    exist: boolean;
+    value: boolean;
+  };
+  virtualarcade: {
+    exist: boolean;
+    value: boolean;
+  };
+  'virtualarcade.includeneogeo': {
+    exist: boolean;
+    value: boolean;
+  };
+  'virtualarcade.hideoriginals': {
+    exist: boolean;
+    value: boolean;
+  };
+  'collection.ports.hide': {
+    exist: boolean;
+    value: boolean;
+  };
+  'collection.lightgun.hide': {
+    exist: boolean;
+    value: boolean;
+  };
+  quicksystemselect: {
+    exist: boolean;
+    value: boolean;
+  };
+  showhelp: {
+    exist: boolean;
+    value: boolean;
+  };
+  'popoup.help': {
+    exist: boolean;
+    value: number;
+  };
+  'popoup.music': {
+    exist: boolean;
+    value: number;
+  };
+  'popoup.netplay': {
+    exist: boolean;
+    value: number;
+  };
+  systemsorting: {
+    exist: boolean;
+    value: string;
+  };
+  'theme.carousel': {
+    exist: boolean;
+    value: boolean;
+  };
+  'theme.transition': {
+    exist: boolean;
+    value: string;
+  };
+  brightness: {
+    exist: boolean;
+    value: number;
+  };
+  showhidden: {
+    exist: boolean;
+    value: boolean;
+  };
+  showonlylatestversion: {
+    exist: boolean;
+    value: boolean;
+  };
+  hidenogames: {
+    exist: boolean;
+    value: boolean;
+  };
+  'arcade.view.enhanced': {
+    exist: boolean;
+    value: boolean;
+  };
+  'arcade.view.hideclones': {
+    exist: boolean;
+    value: boolean;
+  };
+  'arcade.view.hidebios': {
+    exist: boolean;
+    value: boolean;
+  };
+  'arcade.view.hidenonworking': {
+    exist: boolean;
+    value: boolean;
+  };
+  'arcade.usedatabasenames': {
+    exist: boolean;
+    value: boolean;
+  };
+  'virtualarcade.manufacturers': {
+    exist: boolean;
+    value: string[];
+  };
+  debuglogs: {
+    exist: boolean;
+    value: boolean;
+  };
+  'pads.osd': {
+    exist: boolean;
+    value: boolean;
+  };
+  'pads.osd.type': {
+    exist: boolean;
+    value: string;
+  };
+}
 
-export type EmulationStationConfig = z.infer<typeof EmulationStationConfigSchema>
-
-export const EmulationStationConfigOptionsSchema = z.object({
-  bootongamelist: z.object({
-    type: z.string(),
-  }),
-  filteradultgames: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  gamelistonly: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  forcebasicgamelistview: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  menu: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  'collection.lastplayed': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'videosnaps.loop': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  selectedsystem: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  'collection.multiplayer': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'collection.allgames': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'videosnaps.delay': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  hidesystemview: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  clock: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  favoritesonly: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  showgameclipclippingitem: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  showgamecliphelpitems: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'screensaver.time': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  'screensaver.type': z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  'theme.folder': z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  'collection.tate': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  virtualarcade: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.includeneogeo': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.hideoriginals': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'collection.ports.hide': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'collection.lightgun.hide': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  quickselectsystem: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  showhelp: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'popoup.help': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  'popoup.music': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  'popoup.netplay': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  systemsorting: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  'theme.carousel': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'theme.transition': z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  brightness: z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  showhidden: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  showonlylatestversion: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  hidenogames: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'arcade.view.enhanced': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hideclones': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hidebios': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'arcade.view.hidenonworking': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'arcade.usedatabasenames': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'virtualarcade.manufacturers': z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-    displayableStringList: z.array(z.string()),
-  }),
-  debuglogs: z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'pads.osd': z.object({
-    type: z.string(),
-    value: z.boolean(),
-  }),
-  'pads.osd.type': z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-    displayableStringList: z.array(z.string()),
-  }),
-});
-
-export type EmulationStationConfigOptions = z.infer<typeof EmulationStationConfigOptionsSchema>
+export interface EmulationStationConfigOptionsResponse {
+  bootongamelist: {
+    type: string;
+  };
+  filteradultgames: {
+    type: string;
+    value: boolean;
+  };
+  gamelistonly: {
+    type: string;
+    value: boolean;
+  };
+  forcebasicgamelistview: {
+    type: string;
+    value: boolean;
+  };
+  menu: {
+    type: string;
+    allowedStringList: string[];
+  };
+  'collection.lastplayed': {
+    type: string;
+    value: boolean;
+  };
+  'videosnaps.loop': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  selectedsystem: {
+    type: string;
+    allowedStringList: string[];
+  };
+  'collection.multiplayer': {
+    type: string;
+    value: boolean;
+  };
+  'collection.allgames': {
+    type: string;
+    value: boolean;
+  };
+  'videosnaps.delay': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  hidesystemview: {
+    type: string;
+    value: boolean;
+  };
+  clock: {
+    type: string;
+    value: boolean;
+  };
+  favoritesonly: {
+    type: string;
+    value: boolean;
+  };
+  showgameclipclippingitem: {
+    type: string;
+    value: boolean;
+  };
+  showgamecliphelpitems: {
+    type: string;
+    value: boolean;
+  };
+  'screensaver.time': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  'screensaver.type': {
+    type: string;
+    allowedStringList: string[];
+  };
+  'theme.folder': {
+    type: string;
+    allowedStringList: string[];
+  };
+  'collection.tate': {
+    type: string;
+    value: boolean;
+  };
+  virtualarcade: {
+    type: string;
+    value: boolean;
+  };
+  'virtualarcade.includeneogeo': {
+    type: string;
+    value: boolean;
+  };
+  'virtualarcade.hideoriginals': {
+    type: string;
+    value: boolean;
+  };
+  'collection.ports.hide': {
+    type: string;
+    value: boolean;
+  };
+  'collection.lightgun.hide': {
+    type: string;
+    value: boolean;
+  };
+  quickselectsystem: {
+    type: string;
+    value: boolean;
+  };
+  showhelp: {
+    type: string;
+    value: boolean;
+  };
+  'popoup.help': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  'popoup.music': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  'popoup.netplay': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  systemsorting: {
+    type: string;
+    allowedStringList: string[];
+  };
+  'theme.carousel': {
+    type: string;
+    value: boolean;
+  };
+  'theme.transition': {
+    type: string;
+    allowedStringList: string[];
+  };
+  brightness: {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  showhidden: {
+    type: string;
+    value: boolean;
+  };
+  showonlylatestversion: {
+    type: string;
+    value: boolean;
+  };
+  hidenogames: {
+    type: string;
+    value: boolean;
+  };
+  'arcade.view.enhanced': {
+    type: string;
+    value: boolean;
+  };
+  'arcade.view.hideclones': {
+    type: string;
+    value: boolean;
+  };
+  'arcade.view.hidebios': {
+    type: string;
+    value: boolean;
+  };
+  'arcade.view.hidenonworking': {
+    type: string;
+    value: boolean;
+  };
+  'arcade.usedatabasenames': {
+    type: string;
+    value: boolean;
+  };
+  'virtualarcade.manufacturers': {
+    type: string;
+    allowedStringList: string[];
+    displayableStringList: string[];
+  };
+  debuglogs: {
+    type: string;
+    value: boolean;
+  };
+  'pads.osd': {
+    type: string;
+    value: boolean;
+  };
+  'pads.osd.type': {
+    type: string;
+    allowedStringList: string[];
+    displayableStringList: string[];
+  };
+}

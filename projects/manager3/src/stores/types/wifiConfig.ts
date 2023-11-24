cd545cab0af4869423cbccf7b8e6eeff31e8eeef
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 03:06:52 +0100

    feat(webmanager): remove zod for wifi config

--- projects/manager3/src/stores/types/wifiConfig.ts
+++ projects/manager3/src/stores/types/wifiConfig.ts
@@ -1,84 +1,79 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
-import { z } from 'zod';
 
-export const WifiConfigSchema = z.object({
-  enabled: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  netmask: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  key: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  ip: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  ssid: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  gateway: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  region: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  priority: z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  nameservers: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-});
+export interface WifiConfigResponse {
+  enabled: {
+    exist: boolean;
+    value: boolean;
+  };
+  netmask: {
+    exist: boolean;
+    value: string;
+  };
+  key: {
+    exist: boolean;
+    value: string;
+  };
+  ip: {
+    exist: boolean;
+    value: string;
+  };
+  ssid: {
+    exist: boolean;
+    value: string;
+  };
+  gateway: {
+    exist: boolean;
+    value: string;
+  };
+  region: {
+    exist: boolean;
+    value: string;
+  };
+  priority: {
+    exist: boolean;
+    value: number;
+  };
+  nameservers: {
+    exist: boolean;
+    value: string;
+  };
+}
 
-export type WifiConfig = z.infer<typeof WifiConfigSchema>
-
-export const WifiConfigOptionsSchema = z.object({
-  enabled: z.object({
-    type: z.string(),
-  }),
-  netmask: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-  key: z.object({
-    type: z.string(),
-  }),
-  ip: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-  ssid: z.object({
-    type: z.string(),
-  }),
-  gateway: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-  region: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-  priority: z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  nameservers: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-});
-
-export type WifiConfigOptions = z.infer<typeof WifiConfigOptionsSchema>
+export interface WifiConfigOptionsResponse {
+  enabled: {
+    type: string;
+  };
+  netmask: {
+    type: string;
+    allowedChars: string;
+  };
+  key: {
+    type: string;
+  };
+  ip: {
+    type: string;
+    allowedChars: string;
+  };
+  ssid: {
+    type: string;
+  };
+  gateway: {
+    type: string;
+    allowedChars: string;
+  };
+  region: {
+    type: string;
+    allowedStringList: string[];
+  };
+  priority: {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  nameservers: {
+    type: string;
+    allowedChars: string;
+  };
+}

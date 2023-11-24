435c28eee9c3959bc3fb71bd854aa7eb7eae0e05
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 02:24:36 +0100

    feat(webmanager): remove zod for audio config

--- projects/manager3/src/stores/types/audioConfig.ts
+++ projects/manager3/src/stores/types/audioConfig.ts
@@ -1,56 +1,51 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
-import { z } from 'zod';
 
-export const AudioConfigSchema = z.object({
-  bgmusic: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  volume: z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  'music.volume': z.object({
-    exist: z.boolean(),
-    value: z.number(),
-  }),
-  device: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-  mode: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-});
+export interface AudioConfigResponse {
+  bgmusic: {
+    exist: boolean;
+    value: boolean;
+  };
+  volume: {
+    exist: boolean;
+    value: number;
+  };
+  'music.volume': {
+    exist: boolean;
+    value: number;
+  };
+  device: {
+    exist: boolean;
+    value: string;
+  };
+  mode: {
+    exist: boolean;
+    value: string;
+  };
+}
 
-export type AudioConfig = z.infer<typeof AudioConfigSchema>
-
-export const AudioConfigOptionsSchema = z.object({
-  bgmusic: z.object({
-    type: z.string(),
-  }),
-  volume: z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  'music.volume': z.object({
-    type: z.string(),
-    lowerValue: z.number(),
-    higherValue: z.number(),
-  }),
-  device: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-    displayableStringList: z.array(z.string()),
-  }),
-  mode: z.object({
-    type: z.string(),
-    allowedStringList: z.array(z.string()),
-  }),
-});
-
-export type AudioConfigOptions = z.infer<typeof AudioConfigOptionsSchema>
+export interface AudioConfigOptionsResponse {
+  bgmusic: {
+    type: string;
+  };
+  volume: {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  'music.volume': {
+    type: string;
+    lowerValue: number;
+    higherValue: number;
+  };
+  device: {
+    type: string;
+    allowedStringList: string[];
+    displayableStringList: string[];
+  };
+  mode: {
+    type: string;
+    allowedStringList: string[];
+  };
+}

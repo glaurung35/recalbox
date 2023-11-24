6ba5d93a581f4831cbc0f4b7c112a7a638e8578c
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 02:56:31 +0100

    feat(webmanager): remove zod for patreon config

--- projects/manager3/src/stores/types/patreonConfig.ts
+++ projects/manager3/src/stores/types/patreonConfig.ts
@@ -1,22 +1,17 @@
 /**
  * @author Pit64
  */
-import { z } from 'zod';
 
-export const PatreonConfigSchema = z.object({
-  privatekey: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-});
+export interface PatreonConfigResponse {
+  privatekey: {
+    exist: boolean;
+    value: string;
+  };
+}
 
-export type PatreonConfig = z.infer<typeof PatreonConfigSchema>
-
-export const PatreonConfigOptionsSchema = z.object({
-  privatekey: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-});
-
-export type PatreonConfigOptions = z.infer<typeof PatreonConfigOptionsSchema>
+export interface PatreonConfigOptionsResponse {
+  privatekey: {
+    type: string;
+    allowedChars: string;
+  };
+}

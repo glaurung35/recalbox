18ecdf01b32f8e1d2dbc8ef8145e21cfff0c034c
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-11-24 03:04:25 +0100

    feat(webmanager): remove zod for updates config

--- projects/manager3/src/stores/types/updatesConfig.ts
+++ projects/manager3/src/stores/types/updatesConfig.ts
@@ -1,29 +1,24 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
-import { z } from 'zod';
 
-export const UpdatesConfigSchema = z.object({
-  enabled: z.object({
-    exist: z.boolean(),
-    value: z.boolean(),
-  }),
-  type: z.object({
-    exist: z.boolean(),
-    value: z.string(),
-  }),
-});
+export interface UpdatesConfigResponse {
+  enabled: {
+    exist: boolean;
+    value: boolean;
+  };
+  type: {
+    exist: boolean;
+    value: string;
+  };
+}
 
-export type UpdatesConfig = z.infer<typeof UpdatesConfigSchema>
-
-export const UpdatesConfigOptionsSchema = z.object({
-  enabled: z.object({
-    type: z.string(),
-  }),
-  type: z.object({
-    type: z.string(),
-    allowedChars: z.string(),
-  }),
-});
-
-export type UpdatesConfigOptions = z.infer<typeof UpdatesConfigOptionsSchema>
+export interface UpdatesConfigOptionsResponse {
+  enabled: {
+    type: string;
+  };
+  type: {
+    type: string;
+    allowedChars: string;
+  };
+}

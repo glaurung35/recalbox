d065df0465d97721954abd93bbc32c07a3159934
David Barbion <davidb@230ruedubac.fr>
2023-11-29 23:16:52 +0100

    fix(usbmount): avoid double partition mounts

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-usbmount.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-usbmount.sh
@@ -11,9 +11,15 @@
 # 2) for ntfs, if the device is not correctly removed, we've to try to fix (ntfsfix) or fallback in case of error, and the device must not be mounted
 # 3) for some others actions, such as /dev/mmcblk0p3 fs growing, it must not be mounted
 
+if [ "$ACTION" = add ] && mount | grep -q -E "^$DEVNAME\b"; then
+    /usr/bin/recallog -s usbmount -t WARNING "$DEVNAME already mounted, skipping..."
+    exit 1
+fi
+
 # if share is already mounted, just use the basic usbmount
 if test -e /var/run/recalbox.share.mounted
 then
+    /usr/bin/recallog -s usbmount -t INFO "$DEVNAME processed"
     /usr/share/usbmount/usbmount "$1"
     exit $?
 fi
@@ -22,9 +28,9 @@ fi
 # it will be played by the S11share script after the mounting of /recalbox/share
 if mkdir -p /var/run/usbmount.delay
 then
-    N=$(ls /var/run/usbmount.delay | wc -l)
-    let N++
-    set |
-	grep -E '^DEVNAME=|^ID_FS_USAGE=|^ID_FS_UUID=|^ID_FS_TYPE=|^ID_FS_LABEL=' |
-	sed -e s+'^'+'export '+ > /var/run/usbmount.delay/"$N"."$1"
+  set |
+	  grep -E '^DEVNAME=|^ID_FS_USAGE=|^ID_FS_UUID=|^ID_FS_TYPE=|^ID_FS_LABEL=|^ACTION=' |
+	  sed -e s+'^'+'export '+ > /var/run/usbmount.delay/$(basename "$DEVNAME")
+  /usr/bin/recallog -s usbmount -t INFO "$DEVNAME saved for future mount"
 fi
+

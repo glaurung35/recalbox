a7db4cadbff769ea1da6aa8897b45bb13c5c43d4
Bkg2k <bkg2k9@gmail.com>
2023-12-01 15:46:18 +0100

    feat(webmanager): add json custom (array of) object serializers

--- projects/frontend/es-app/src/web/server/handlers/RequestHandler.cpp
+++ projects/frontend/es-app/src/web/server/handlers/RequestHandler.cpp
@@ -557,26 +557,30 @@ void RequestHandler::RomsGetList(const Rest::Request& request, Http::ResponseWri
   SystemData* system = mSystemManager.SystemByName(systemName);
   FileData::List allGames = system->getAllGames();
 
+  class Serializer : public ISerializeToJson<const FileData>
+  {
+    public:
+      // Serialize a single game
+      JSONBuilder Serialize(const FileData* game) override
+      {
+        if (game->IsGame())
+          return JSONBuilder()
+                 .Open()
+                   .Field("path", game->Metadata().Rom().ToString())
+                   .Field("name", game->Metadata().Name())
+                   .Field("publisher", game->Metadata().Publisher())
+                   .Field("developer", game->Metadata().Developer())
+                   .Field("genre", game->Metadata().Genre())
+                   .Field("players", game->Metadata().PlayerMax())
+                   .Field("rating", game->Metadata().Rating())
+                 .Close();
+        return JSONBuilder();
+      }
+  } serializer;
+
   JSONBuilder roms;
   roms.Open()
-      .OpenObject("roms");
-
-  for(FileData* game : allGames)
-  {
-    if (game->IsGame())
-    {
-      roms.Open()
-          .Field("name", game->Metadata().Name())
-          .Field("publisher", game->Metadata().Publisher())
-          .Field("developer", game->Metadata().Developer())
-          .Field("genre", game->Metadata().Genre())
-          .Field("players", game->Metadata().PlayerMax())
-          .Field("rating", game->Metadata().Rating())
-          .Close();
-    }
-  }
-
-  roms.CloseObject()
+      .Field<FileData>("roms", allGames, serializer)
       .Close();
 
   RequestHandlerTools::Send(response, Http::Code::Ok, roms, Mime::Json);

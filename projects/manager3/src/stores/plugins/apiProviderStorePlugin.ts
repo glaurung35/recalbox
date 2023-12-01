90e80f78d817d7d8d9e6d959250d623a35768126
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-01 23:36:57 +0100

    feat(webmanager): add mqtt es events subscription

--- projects/manager3/src/stores/plugins/apiProviderStorePlugin.ts
+++ projects/manager3/src/stores/plugins/apiProviderStorePlugin.ts
@@ -4,6 +4,10 @@
 import { api } from 'boot/axios';
 import { AxiosInstance } from 'axios';
 
+export interface ApiProviderStore {
+  _apiProvider: AxiosInstance;
+}
+
 // eslint-disable-next-line consistent-return
 const ApiProviderStorePlugin = (): {_apiProvider: AxiosInstance} => ({
   _apiProvider: api,

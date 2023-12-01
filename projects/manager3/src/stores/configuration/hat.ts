ad146829c18e4eae1ff092dd21907607a83ada17
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-01 23:36:57 +0100

    feat(webmanager): add mqtt es events subscription

--- projects/manager3/src/stores/configuration/hat.ts
+++ projects/manager3/src/stores/configuration/hat.ts
@@ -3,16 +3,19 @@
  */
 import { defineStore } from 'pinia';
 import { CONFIGURATION } from 'src/router/api.routes';
+import { FetchOptionsStore } from 'stores/plugins/fetchOptionsStorePlugin';
+import { FetchStore } from 'stores/plugins/fetchStorePlugin';
+import { PostStore } from 'stores/plugins/postStorePlugin';
 import {
   HatConfigOptionsResponse,
   HatConfigResponse,
 } from 'stores/types/hat';
 
-export type HatStoreState = {
-  _baseUrl: string,
-  _hatOptions: HatConfigOptionsResponse,
-  hat: HatConfigResponse,
-};
+export interface HatStoreState extends FetchStore, PostStore, FetchOptionsStore {
+  _baseUrl: string;
+  _hatOptions: HatConfigOptionsResponse;
+  hat: HatConfigResponse;
+}
 
 export const useHatStore = defineStore('hat', {
   state: () => ({

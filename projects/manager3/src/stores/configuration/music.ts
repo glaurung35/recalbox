a08946671fe835175feca30bc95c2386c7326a79
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-01 23:36:57 +0100

    feat(webmanager): add mqtt es events subscription

--- projects/manager3/src/stores/configuration/music.ts
+++ projects/manager3/src/stores/configuration/music.ts
@@ -3,16 +3,18 @@
  */
 import { defineStore } from 'pinia';
 import { CONFIGURATION } from 'src/router/api.routes';
+import { FetchStore } from 'stores/plugins/fetchStorePlugin';
+import { PostStore } from 'stores/plugins/postStorePlugin';
 import {
   MusicConfigOptionsResponse,
   MusicConfigResponse,
 } from 'stores/types/music';
 
-export type MusicStoreState = {
-  _baseUrl: string,
-  _musicOptions: MusicConfigOptionsResponse,
-  music: MusicConfigResponse,
-};
+export interface MusicStoreState extends FetchStore, PostStore {
+  _baseUrl: string;
+  _musicOptions: MusicConfigOptionsResponse;
+  music: MusicConfigResponse;
+}
 
 export const useMusicStore = defineStore('music', {
   state: () => ({

9e739bb56d67f0318eae7961de685aa2e9d68400
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-08 12:30:23 +0100

    feat(webmanager): add current game fetch

--- projects/manager3/src/stores/roms.ts
+++ projects/manager3/src/stores/roms.ts
@@ -2,21 +2,23 @@
  * @author Nicolas TESSIER aka Asthonishia
  */
 import { defineStore } from 'pinia';
-import { ROMS, SYSTEMS } from 'src/router/api.routes';
-import { ApiProviderStore } from 'stores/plugins/apiProviderStorePlugin';
+import { getPathBy, ROMS, SYSTEMS } from 'src/router/api.routes';
+import { Rom } from 'src/stores/types/roms';
 
-export interface RomsStoreState extends ApiProviderStore {
-  roms: object;
+export interface RomsStoreState {
+  roms: Rom[],
 }
 
 export const useRomsStore = defineStore('roms', {
   state: () => ({
-    roms: {},
+    roms: [],
   } as RomsStoreState),
 
   actions: {
     async fetch() {
       try {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
         const response = await this._apiProvider.get(ROMS.all);
         this.roms = response.data;
       } catch (error) {
@@ -26,8 +28,10 @@ export const useRomsStore = defineStore('roms', {
     },
     async fetchBySystem(system: string) {
       try {
-        const response = await this._apiProvider.get(`${SYSTEMS.root}/${system}/roms`);
-        this.roms = response.data;
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const response = await this._apiProvider.get(getPathBy(SYSTEMS.roms, system));
+        this.roms = response.data.roms;
       } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);

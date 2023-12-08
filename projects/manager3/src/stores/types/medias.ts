f779c4c4471259cbd8dba4203c38ec64a0f7c410
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-08 12:30:23 +0100

    feat(webmanager): add current game fetch

--- projects/manager3/src/stores/types/medias.ts
+++ projects/manager3/src/stores/types/medias.ts
@@ -1,11 +1,18 @@
 /**
  * @author Nicolas TESSIER aka Asthonishia
  */
+export enum Type {
+  image= 'image',
+  webm = 'video/webm',
+  xMsvideo = 'video/x-msvideo',
+  mp4 = 'video/mp4'
+}
+
 export interface Screenshot {
   name: string;
   path: string;
   date: string;
-  type: 'image'|'video';
+  type: Type;
 }
 
 export interface MediasResponse {

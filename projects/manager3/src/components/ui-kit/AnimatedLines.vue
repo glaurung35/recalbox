a0aa16c87464f6603fa1d7e14070607b9652b66e
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-09 22:40:31 +0100

    fix(webmanager): add SystemView animated lines

---
+++ projects/manager3/src/components/ui-kit/AnimatedLines.vue
@@ -0,0 +1,59 @@
+<!--
+@author Nicolas TESSIER aka Asthonishia
+-->
+<template>
+  <div class="lines">
+    <div
+      v-for="(color, index) in colors"
+      :class="'line ' + index"
+      :key="color"
+      :style="{ backgroundColor: color }"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+  colors: { type: Object, required: true },
+});
+const { colors } = toRefs(props);
+</script>
+
+<style lang="sass">
+.lines
+  position: absolute
+  display: flex
+  flex-direction: row
+  top: -1em
+  left: 15px
+  width: 6%
+  height: calc(100% + 1em)
+  opacity: 0.3
+
+  .line
+    animation-name: grow
+    width: 20%
+    height: 0
+    animation-duration: .4s
+    animation-timing-function: ease-in
+    border-bottom-left-radius: 5px
+    border-bottom-right-radius: 5px
+    animation-fill-mode: forwards
+
+    @keyframes grow
+      from
+        height: 0
+      to
+        height: 110%
+
+  .band1
+    animation-delay: 250ms
+  .band2
+    animation-delay: 350ms
+  .band3
+    animation-delay: 450ms
+  .band4
+    animation-delay: 550ms
+</style>

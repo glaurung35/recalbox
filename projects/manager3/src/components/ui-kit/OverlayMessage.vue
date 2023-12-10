aa35ba6ae45f01f682b71113bcb7d14660c254ab
Nicolas TESSIER <nicolas.tessier@thetribe.io>
2023-12-10 23:15:48 +0100

    feat(webmanager): add mqtt fetch es status on connect

---
+++ projects/manager3/src/components/ui-kit/OverlayMessage.vue
@@ -0,0 +1,44 @@
+<!--
+@author Nicolas TESSIER aka Asthonishia
+-->
+<template>
+  <div :class="classes">
+    <q-icon :name="icon"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, toRefs } from 'vue';
+
+const props = defineProps({
+  background: { type: Boolean },
+  icon: { type: String, required: true },
+});
+
+const {
+  background,
+  icon,
+} = toRefs(props);
+
+const classes = computed(() => `overlayMessage${background.value ? ' background' : ''}`);
+</script>
+
+<style lang="sass">
+.background
+  background: #F4F5F7BA
+
+.overlayMessage
+  position: absolute
+  left: 0
+  top: -1em
+  width: 100%
+  height: calc(100% + 1em)
+  z-index: 9999
+  display: flex
+  align-items: center
+  justify-content: center
+
+  .q-icon
+    color: $light-blue
+    font-size: 9em
+</style>

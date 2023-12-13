aac2b35f2afa1e37911181b5746004dcdfc1cd4b
Pit64 <yrigaud@icloud.com>
2023-12-13 23:05:15 +0100

    feat(webmanager): add routes to get theme values

---
+++ projects/manager3/src/components/layout/MobileHelpButton.vue
@@ -0,0 +1,107 @@
+<!--
+@author Nicolas TESSIER aka Asthonishia
+-->
+<template>
+  <q-fab
+    flat
+    square
+    color="secondary"
+    direction="down"
+    icon="mdi-cog"
+    vertical-actions-align="right"
+    @update:model-value="update"
+    :model-value="model"
+  >
+    <q-fab-action
+      @click="useServerStore().supportArchive()"
+      color="secondary"
+      icon="mdi-bash"
+      :label="$t('general.server.supportArchive.title')"
+      label-position="left"
+      square
+    />
+    <q-fab-action
+      @click="() => open = true"
+      color="secondary"
+      icon="mdi-source-branch"
+      label="Versions"
+      label-position="left"
+      square
+    />
+    <q-fab-action
+      @click="useServerStore().reboot()"
+      color="secondary"
+      icon="mdi-restart"
+      :label="$t('general.server.reboot.title')"
+      label-position="left"
+      square
+    />
+    <q-fab-action
+      @click="useServerStore().shutdown()"
+      color="secondary"
+      icon="mdi-stop"
+      :label="$t('general.server.shutdown.title')"
+      label-position="left"
+      square
+    />
+  </q-fab>
+  <q-dialog
+    transition-hide="slide-down"
+    transition-show="slide-up"
+    v-model="open"
+    class="versions-dialog"
+  >
+    <q-card
+      class="bg-primary text-white background versions-dialog-card"
+    >
+      <q-card-section>
+        <VersionsList/>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref, toRefs } from 'vue';
+import VersionsList from 'components/layout/VersionsList.vue';
+import { useVersionsStore } from 'stores/versions';
+import { useServerStore } from 'stores/server';
+
+useVersionsStore().fetch();
+
+const props = defineProps({
+  model: { type: Boolean, required: true },
+});
+
+const { model } = toRefs(props);
+
+const open = ref<boolean>(false);
+const emit = defineEmits(['toggle']);
+
+function update(value: boolean) {
+  emit('toggle', { label: 'helpButton', value });
+}
+</script>
+
+<style lang="sass">
+.versions-dialog
+  .versions-dialog-card
+    border-left: 6px solid $accent
+
+.q-page-container
+  #help-button
+    .q-btn--fab-mini:hover
+      .q-icon:before
+        color: $accent
+
+    .gitbook
+      i
+        font-size: 18px
+        margin-left: 3px
+        margin-right: 3px
+
+  #help-button.q-fab--opened
+    .q-focus-helper
+      background: currentColor
+      opacity: 0.15
+</style>

18006fed26eb60814d9ed566a436fde50e9ab23f
digitalLumberjack <digitallumberjack@gmail.com>
2023-12-31 13:55:58 +0100

    feat(frontend): rrgbj: fix neogeo layouts and dreamcast layout

--- projects/frontend/es-app/src/emulators/run/JammaData.h
+++ projects/frontend/es-app/src/emulators/run/JammaData.h
@@ -13,28 +13,41 @@
 class JammaData
 {
   private:
-    bool systemIsDreamcastArcade(const std::string& systemName) const
+    bool systemIsDreamcastArcade(const String& systemName) const
     {
       return systemName == "atomiswave" || systemName == "naomigd" || systemName == "naomi";
     }
+
+    bool systemIsArcade(const FileData& game, const EmulatorData& emulator) const
+    {
+      return systemIsDreamcastArcade(game.System().Name())
+              || (emulator.Emulator() == "libretro" && (emulator.Core() == "fbneo" || emulator.Core() == "mame2015" || emulator.Core() == "mame"));
+    }
+
   public:
     JammaData() {};
     bool ShouldConfigureJammaConfiggen() const {
-      return  (Board::Instance().CrtBoard().GetCrtAdapter() == CrtAdapterType::RGBJamma ||
-               Board::Instance().CrtBoard().GetCrtAdapter() == CrtAdapterType::RGBJammaV2);
+      return  (Board::Instance().CrtBoard().GetCrtAdapter() == CrtAdapterType::RGBJamma);
     }
 
     bool ShouldSwitchTo6ButtonLayout(const FileData& game, const EmulatorData& emulator) const {
-      return  ShouldConfigureJammaConfiggen() && CrtConf::Instance().GetSystemCRTJammaPanelButtons() == "6" &&
-               !  (emulator.Emulator() == "libretro" &&
-               (emulator.Core() == "fbneo" || emulator.Core() == "mame2015" || emulator.Core() == "mame" || systemIsDreamcastArcade(game.System().Name()))
-               );
+      return  ShouldConfigureJammaConfiggen()
+              && CrtConf::Instance().GetSystemCRTJammaPanelButtons() == "6"
+              && ! systemIsArcade(game, emulator);
     }
-    std::string JammaControlType(const FileData& game, const EmulatorData& emulator) const {
-      if(ShouldSwitchTo6ButtonLayout(game, emulator)) {
+
+    bool ShouldConfigureNeoGeoLayout(const FileData& game, const EmulatorData& emulator) const {
+      String buttonOnPanel = CrtConf::Instance().GetSystemCRTJammaPanelButtons();
+      return  ShouldConfigureJammaConfiggen()
+              && (buttonOnPanel == "6" || buttonOnPanel == "5")
+              && systemIsArcade(game, emulator);
+    }
+
+    String JammaControlType(const FileData& game, const EmulatorData& emulator) const {
+      if(ShouldSwitchTo6ButtonLayout(game, emulator))
         return "6btns";
-      } else {
-        return CrtConf::Instance().GetSystemCRTJammaNeogeoLayout();
-      }
+      else if(ShouldConfigureNeoGeoLayout(game, emulator))
+          return CrtConf::Instance().GetSystemCRTJammaNeogeoLayout();
+      else return "standard";
     }
 };

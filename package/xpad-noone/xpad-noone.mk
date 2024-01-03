fd66eada394cc5b74250546891a54da3a8bc5647
Pit64 <yrigaud@icloud.com>
2024-01-03 11:07:29 +0100

    fix(controllers): fix xpad-noone repository url

--- package/xpad-noone/xpad-noone.mk
+++ package/xpad-noone/xpad-noone.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
+# Commit of 2022/12/27
 XPAD_NOONE_VERSION = d02737f7eab1e17a7748fbe550dd982e3808525d
-XPAD_NOONE_SITE = $(call github,medusalix-os,xpad-noone,$(XPAD_NOONE_VERSION))
+XPAD_NOONE_SITE = $(call github,medusalix,xpad-noone,$(XPAD_NOONE_VERSION))
 XPAD_NOONE_LICENSE = GPL-2.0
 
 $(eval $(kernel-module))

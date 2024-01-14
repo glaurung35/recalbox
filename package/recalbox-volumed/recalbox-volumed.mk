9aa3c045bec686bf5da9a5ced491577eb68639ce
digitalLumberjack <digitallumberjack@gmail.com>
2024-01-14 10:57:39 +0100

    feat(system): add recalbox-volumed in mono repo

--- package/recalbox-volumed/recalbox-volumed.mk
+++ package/recalbox-volumed/recalbox-volumed.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-RECALBOX_VOLUMED_VERSION = 0dc27498eb4e5503921128367fe86b103a983f2c
-RECALBOX_VOLUMED_SITE = https://gitlab.com/recalbox/recalbox-volumed.git
-RECALBOX_VOLUMED_SITE_METHOD = git
+RECALBOX_VOLUMED_VERSION = custom
+RECALBOX_VOLUMED_SITE = $(TOPDIR)/../projects/recalbox-volumed
+RECALBOX_VOLUMED_SITE_METHOD = local
 RECALBOX_VOLUMED_LICENSE = GPL-3.0
 RECALBOX_VOLUMED_LICENSE_FILES = LICENSE
 RECALBOX_VOLUMED_DEPENDENCIES = pulseaudio

f635629e559774b1e9dc8b444aa2a60f473f5c6b
digitalLumberjack <digitallumberjack@gmail.com>
2024-01-26 22:50:26 +0100

    feat(system): remove me: mount exfat in sync
    remove this because it may have sever impact on writings

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-mount.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-mount.sh
@@ -28,7 +28,7 @@ mountOptions() {
   case "${filesystemType}" in
     "vfat") echo "${defaultMountOptions},${windowsMask},iocharset=utf8,flush" ;;
     "ntfs") echo "${defaultMountOptions},${windowsMask}" ;;
-    "exfat") echo "${defaultMountOptions},${windowsMask}" ;;
+    "exfat") echo "${defaultMountOptions},${windowsMask},sync" ;;
     *) echo "${defaultMountOptions}"
   esac
 }

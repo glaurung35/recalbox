461bc5c9f0e9e5df8ab4e594e0c87ce1bd73c8bc
Bkg2k <bkg2k9@gmail.com>
2024-01-30 08:27:35 +0100

    feat(frontend): add coloured corners in image component

--- projects/frontend/es-core/src/components/ImageComponent.h
+++ projects/frontend/es-core/src/components/ImageComponent.h
@@ -3,6 +3,7 @@
 #include "components/base/ThemableComponent.h"
 #include <memory>
 #include "resources/TextureResource.h"
+#include "utils/gl/Colors.h"
 
 class ImageComponent : public ThemableComponent
 {
@@ -48,10 +49,6 @@ class ImageComponent : public ThemableComponent
     void setImage(const std::shared_ptr<TextureResource>& texture);
 
     [[nodiscard]] Path getImagePath() const { return mPath; };
-
-    void onSizeChanged() override;
-
-    void setOpacity(unsigned char opacity) override;
 
     [[nodiscard]] Vector2f getTargetSize() const { return mTargetSize; }
 
@@ -130,7 +127,6 @@ class ImageComponent : public ThemableComponent
       {
         mTopAlpha = top;
         mBottomAlpha = bottom;
-        updateColors();
       }
     }
 
@@ -140,6 +136,100 @@ class ImageComponent : public ThemableComponent
      * @param v Vector containing top alpha in x and bottom alpha in y
      */
     void SetReflection(const Vector2f& v) { SetReflection(v.x(), v.y()); }
+
+    /*!
+     * @brief Set box 4 corners color
+     * @param color RGBA color
+     */
+    void SetColor(Colors::ColorRGBA color) { setColorShift(color); }
+
+    /*!
+     * @brief Set box top border color
+     * @param color RGBA color
+     */
+    void SetTopColor(Colors::ColorRGBA color)
+    {
+      mTopLeftColor = color;
+      mTopRightColor = color;
+    }
+
+    /*!
+     * @brief Set box bottom border color
+     * @param color RGBA color
+     */
+    void SetBottomColor(Colors::ColorRGBA color)
+    {
+      mBottomLeftColor = color;
+      mBottomRightColor = color;
+    }
+
+    /*!
+     * @brief Set box left border color
+     * @param color RGBA color
+     */
+    void SetLeftColor(Colors::ColorRGBA color)
+    {
+      mTopLeftColor = color;
+      mBottomLeftColor = color;
+    }
+
+    /*!
+     * @brief Set box right border color
+     * @param color RGBA color
+     */
+    void SetRightColor(Colors::ColorRGBA color)
+    {
+      mTopRightColor = color;
+      mBottomRightColor = color;
+    }
+
+    /*!
+     * @brief Set box top/left corner color
+     * @param color RGBA color
+     */
+    void SetTopLeftColor(Colors::ColorRGBA color) { mTopLeftColor = color; }
+
+    /*!
+     * @brief Set box top/right corner color
+     * @param color RGBA color
+     */
+    void SetTopRightColor(Colors::ColorRGBA color) { mTopRightColor = color; }
+
+    /*!
+     * @brief Set box bottom/left corner color
+     * @param color RGBA color
+     */
+    void SetBottomLeftColor(Colors::ColorRGBA color) { mBottomLeftColor = color; }
+
+    /*!
+     * @brief Set box bottom/right corner color
+     * @param color RGBA color
+     */
+    void SetBottomRightColor(Colors::ColorRGBA color) { mBottomRightColor = color; }
+
+    /*!
+     * @brief Get box top/left color
+     * @return Box top/left color
+     */
+    [[nodiscard]] Colors::ColorRGBA TopLeftColor() const { return mTopLeftColor; }
+
+    /*!
+     * @brief Get box top/right color
+     * @return Box top/right color
+     */
+    [[nodiscard]] Colors::ColorRGBA TopRightColor() const { return mTopRightColor; }
+
+    /*!
+     * @brief Get box bottom/left color
+     * @return Box bottom/left color
+     */
+    [[nodiscard]] Colors::ColorRGBA BottomLeftColor() const { return mBottomLeftColor; }
+
+    /*!
+     * @brief Get box bottom/right color
+     * @return Box bottom/right color
+     */
+    [[nodiscard]] Colors::ColorRGBA BottomRightColor() const { return mBottomRightColor; }
 
   private:
     Vector2f mTargetSize;
@@ -151,22 +241,14 @@ class ImageComponent : public ThemableComponent
     // Used internally whenever the resizing parameters or texture change.
     void resize();
 
-    struct Vertex
-    {
-      Vector2f pos;
-      Vector2f tex;
-    } mVertices[12];
-
-    GLubyte mColors[12 * 4];
-
-    void updateVertices();
-
-    void updateColors();
-
     void fadeIn(bool textureLoaded);
 
-    unsigned int mColorShift;
-    unsigned int mOriginColor;
+    //! Color
+    Colors::ColorRGBA mTopLeftColor;
+    Colors::ColorRGBA mTopRightColor;
+    Colors::ColorRGBA mBottomLeftColor;
+    Colors::ColorRGBA mBottomRightColor;
+    Colors::ColorRGBA mOriginColor;
 
     std::shared_ptr<TextureResource> mTexture;
     unsigned char mFadeOpacity;

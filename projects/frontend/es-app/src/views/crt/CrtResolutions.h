d1c9cbd44342edaf1b8255b7aa342b15c8cf7641
digitalLumberjack <digitallumberjack@gmail.com>
2024-01-31 11:28:51 +0100

    feat(system): crt: enhance calibration screen
    - offset limit for all modes in UI
    - add horizontal limit in kernel module
    - add specific calibration sequency for MultiSync mode

--- projects/frontend/es-app/src/views/crt/CrtResolutions.h
+++ projects/frontend/es-app/src/views/crt/CrtResolutions.h
@@ -6,7 +6,7 @@
 //
 #pragma once
 
-enum class CrtResolution
+enum CrtResolution: uint
 {
   rNone, //!< Null element
   r224p, //!< PAL + NTSC 224p
@@ -19,4 +19,28 @@ enum class CrtResolution
   r320x240p, //!< 320x240
   r384x288p, //!< 384x288
   _rCount, //!< Counter
+};
+
+enum Offset: uint
+{
+  Left,
+  Right,
+  Up,
+  Down,
+  Count
+};
+
+static int porch[CrtResolution::_rCount][Offset::Count]
+{
+  // { hback / ratio (max 20), hfront_porch / ratio, Up , Down }
+  {0,0,0,0}, // rNone, //!< Null element
+  {-20,12,-15,10}, // r224p, //!< PAL + NTSC 224p
+  {-20,12,-16,1}, // r240p, //!< PAL + NTSC 224p
+  {-20,12,-16,4}, // r288p, //!< PAL only
+  {-15,4,-16,3}, // r480i, //!< NTSC interlaced hires
+  {-12,4,-12,6}, // r576i, //!< PAL interlaced hires
+  {-8,4,-10,10}, // r480p, //!< 31kHz
+  {-20,8,-14,4}, // r240p120Hz, //!< 31kHz 240
+  {-7,1,-14,4}, // r320x240p, //!< 320x240
+  {-6,2,-12,3}, // r384x288p, //!< 384x288
 };

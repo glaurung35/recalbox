76f30d99b7b6a0bd56fffa7d16e8f2376a406a1d
Санька Четвёртый <cahbka-iv@mail.ru>
2024-05-27 15:50:34 +0000

    Workaround for sunshine streamer which doesn't fill game short name.

--- package/moonlight-qt/scripts/Moonlight.sh
+++ package/moonlight-qt/scripts/Moonlight.sh
@@ -145,7 +145,7 @@ function mlInit {
   do
     gameId=$(echo $line | cut -d "$moonlightSeparator" -f 1)
     gameShortName=$(echo $line | cut -d "$moonlightSeparator" -f 2)
-    [ -z "$gameShortName" ] && $(echo gameId)
+    [ -z "$gameShortName" ] && $(echo $gameId)
     gameLongName=$(echo $line | cut -d "$moonlightSeparator" -f 3)
     romFileName=$(gameShortNameToFileName $gameShortName $hostOrIP)
     [ ! -z "$hostOrIP" ] && romFileName="${hostOrIP}/${romFileName}"

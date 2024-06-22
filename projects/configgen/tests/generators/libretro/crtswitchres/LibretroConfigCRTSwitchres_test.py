4b12812a863c447b3ad9e2e84023e377dd3f1673
digitalLumberjack <digitallumberjack@gmail.com>
2024-06-22 18:01:18 +0200

    feat(system): crt: add 31kHz support for crt
    - ✅ doubled resolution for all games in 31kHz
    - ✅ 240p@120hz supported with original resolution and doubled refresh
      rate
    - ✅ resolution selection for 31kHz screens
    - ✅ scanlines support

--- projects/configgen/tests/generators/libretro/crtswitchres/LibretroConfigCRTSwitchres_test.py
+++ projects/configgen/tests/generators/libretro/crtswitchres/LibretroConfigCRTSwitchres_test.py
@@ -6,7 +6,11 @@ from configgen.generators.libretro.crtswitchres.LibretroConfigCRTSwitchres impor
 from configgen.settings.keyValueSettings import keyValueSettings
 
 
-def configureForCrt(emulator: Emulator, crtvideostandard="auto", crtresolutiontype="progressive", crtscreentype="15kHz",
+@pytest.fixture(autouse=True)
+def before(mocker):
+    mocker.patch('configgen.utils.architecture.Architecture.isPi5', return_value=False, new_callable=mocker.PropertyMock)
+
+def configureForCrt(emulator: Emulator, crtvideostandard="auto", crtresolutiontype="auto", crtscreentype="15kHz",
                     crtadaptor="recalboxrgbdual", crtregion="auto", crtscanlines="", rotation=0, verticalgame=False,
                     crtsuperrez = "1"
                     ):
@@ -57,7 +61,7 @@ def test_given_rpi5_should_disable_interlaced(mocker):
 
 def test_given_rpi4_should_not_disable_interlaced(mocker):
     mocker.patch('configgen.utils.architecture.Architecture.isPi5', return_value=False, new_callable=mocker.PropertyMock)
-    #mocker.patch('configgen.utils.architecture.Architecture', **{'isPi5.return_value': False})
+
     emulator = configureForCrt(
         Emulator(name='snes', videoMode='1920x1080', ratio='auto', emulator='libretro', core='snes9x'),
         crtresolutiontype="progressive", crtscreentype="15kHz", crtadaptor="recalboxrgbdual", crtsuperrez="0")
@@ -90,3 +94,81 @@ def test_given_yoko_game_on_tate_screen_should_rotate(mocker):
     config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
 
     assert config_lines["video_rotation"] == 3
+
+
+def test_given_15kHz_screen_and_auto_resolution_then_configure_switchres(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="15kHz", crtresolutiontype="auto", crtsuperrez="1920")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 1
+    assert config_lines["crt_switch_resolution_super"] == "1920"
+    assert config_lines["crt_switch_resolution_no_interlaced"] == 0
+
+def test_given_15kHz_screen_and_240p_resolution_then_avoid_interlacd_modes(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="15kHz", crtresolutiontype="240p", crtsuperrez="1920")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 1
+    assert config_lines["crt_switch_resolution_super"] == "1920"
+    assert config_lines["crt_switch_resolution_no_interlaced"] == 1
+
+def test_given_31kHz_screen_and_auto_resolution_then_configure_switchres(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="31kHz", crtresolutiontype="auto", crtsuperrez="1920")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 2
+    assert config_lines["crt_switch_resolution_super"] == "1920"
+    assert config_lines["crt_switch_resolution_no_interlaced"] == 0
+
+def test_given_31kHz_screen_and_doublefreq_resolution_then_configure_switchres(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="31kHz", crtresolutiontype="doublefreq")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 3
+
+
+def test_given_31kHz_yoko_screen_and_yoko_game_480p_and_scanlines_then_configure_scanlines(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="31kHz", crtresolutiontype="480p", crtscanlines="heavy")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 2
+    assert config_lines["video_shader_enable"] == '"true"'
+    assert config_lines["video_shader_dir"] == '"/recalbox/share/shaders/"'
+    assert config_lines["video_shader"] == '/recalbox/share/shaders/rrgbd-scanlines-heavy.glslp'
+
+def test_given_31kHz_tate_screen_and_yoko_game_480p_and_scanlines_then_skip_scanlines(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="31kHz", crtresolutiontype="480p", crtscanlines="heavy", verticalgame=False, rotation=3)
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 2
+    assert config_lines["video_shader_enable"] == '"false"'
+
+def test_given_31kHz_yoko_screen_and_tate_game_480p_and_scanlines_then_skip_scanlines(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="31kHz", crtresolutiontype="480p", crtscanlines="heavy", verticalgame=True, rotation=0)
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 2
+    assert config_lines["video_shader_enable"] == '"false"'
+
+def test_given_15kHz_and_scanlines_then_skip_scanlines(mocker):
+    emulator = configureForCrt(
+        Emulator(name='fbneo', videoMode='1920x1080', ratio='auto', emulator='libretro', core='fbneo'),
+        crtscreentype="15kHz", crtresolutiontype="auto", crtscanlines="heavy")
+    config_lines = LibretroConfigCRTSwitchres().createConfigFor(emulator, "Mario.smc")
+
+    assert config_lines["crt_switch_resolution"] == 1
+    assert config_lines["video_shader_enable"] == '"false"'

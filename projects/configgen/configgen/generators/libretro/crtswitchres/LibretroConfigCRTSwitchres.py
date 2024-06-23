abb68c4f2e2256ddf4bbf74f33935436d6a6ae29
digitalLumberjack <digitallumberjack@gmail.com>
2024-06-23 11:56:53 +0200

    feat(system): crt: add trisync and 24kHz support for crt
    - ✅ supported screens : 15kHz, 15+24kHz, 15+31kHz, 31kHz, 15+24+31kHz
    - ✅ automatic best mode selection by default
    - ✅ resolution selection at game startup if enabled in options
    - ✅ naomi runs well in 384p on 24khz
    - ✅ 24kHz games can be forced in 240p

--- projects/configgen/configgen/generators/libretro/crtswitchres/LibretroConfigCRTSwitchres.py
+++ projects/configgen/configgen/generators/libretro/crtswitchres/LibretroConfigCRTSwitchres.py
@@ -1,7 +1,7 @@
 import typing
 
 from configgen.Emulator import Emulator
-from configgen.crtswitchres.CRTTypes import CRTResolutionType, CRTScreenType, CRTScanlines
+from configgen.crtswitchres.CRTTypes import CRTResolutionType, CRTScreenType, CRTScanlines, RetroarchScreenTypeIndex
 from configgen.utils.Rotation import Rotation
 from configgen.utils.architecture import Architecture
 
@@ -9,11 +9,13 @@ from configgen.utils.architecture import Architecture
 class LibretroConfigCRTSwitchres:
 
     def manage_scanlines(self, system: Emulator, config: typing.Dict[str, str]):
-        if system.CRTScreenType == CRTScreenType.k31 and system.CRTScanlines != CRTScanlines.NONE and (system.CRTResolutionType == CRTResolutionType.Force480p or system.CRTResolutionType == CRTResolutionType.Auto):
-            if (system.Rotation.isTate() and system.VerticalGame) or (not system.Rotation.isTate() and not system.VerticalGame):
-                config.update({"video_shader_enable": '"true"'})
-                config.update({"video_shader_dir": '"/recalbox/share/shaders/"'})
-                config.update({"video_shader": '/recalbox/share/shaders/rrgbd-scanlines-{}.glslp'.format(system.CRTScanlines)})
+        if system.CRTScanlines != CRTScanlines.NONE:
+            if (system.CRTScreenType == CRTScreenType.kHz31 and (system.CRTResolutionType == CRTResolutionType.Force480p or system.CRTResolutionType == CRTResolutionType.Auto)) \
+                    or (system.CRTScreenType == CRTScreenType.kHzTriFreq or system.CRTScreenType == CRTScreenType.kHzMulti1531) and system.CRTResolutionType == CRTResolutionType.Force480p:
+                if (system.Rotation.isTate() and system.VerticalGame) or (not system.Rotation.isTate() and not system.VerticalGame):
+                    config.update({"video_shader_enable": '"true"'})
+                    config.update({"video_shader_dir": '"/recalbox/share/shaders/"'})
+                    config.update({"video_shader": '/recalbox/share/shaders/rrgbd-scanlines-{}.glslp'.format(system.CRTScanlines)})
 
     def createConfigFor(self, system: Emulator, rom: str) -> typing.Dict[str, str]:
         config: typing.Dict[str, any] = {"aspect_ratio_index": "22",
@@ -52,13 +54,30 @@ class LibretroConfigCRTSwitchres:
                                          }
 
         match system.CRTScreenType:
-            case CRTScreenType.k15:
-                config["crt_switch_resolution"] = 1
-            case CRTScreenType.k31:
+            case CRTScreenType.kHz15:
+                config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz15.value
+            case CRTScreenType.kHz31:
                 if system.CRTResolutionType == CRTResolutionType.DoubleFreq:
-                    config["crt_switch_resolution"] = 3
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz31at120.value
                 else:
-                    config["crt_switch_resolution"] = 2
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz31.value
+            case CRTScreenType.kHzMulti1531:
+                if system.CRTResolutionType == CRTResolutionType.Force480p:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz31.value
+                else:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHzMulti1531.value
+            case CRTScreenType.kHzMulti1525:
+                if system.CRTResolutionType == CRTResolutionType.Force240p:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz15.value
+                else:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHzMulti1525.value
+            case CRTScreenType.kHzTriFreq:
+                if system.CRTResolutionType == CRTResolutionType.Force240p:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz15.value
+                elif system.CRTResolutionType == CRTResolutionType.Force480p:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHz31.value
+                else:
+                    config["crt_switch_resolution"] = RetroarchScreenTypeIndex.kHzTriFreq.value
 
         config["crt_switch_resolution_super"] = system.CRTSuperrez
         pi5 = Architecture().isPi5
@@ -68,7 +87,7 @@ class LibretroConfigCRTSwitchres:
             config["crt_switch_resolution_no_interlaced"] = 0
 
         if system.Rotation.isTate():
-                config["video_rotation"] = system.Rotation
+            config["video_rotation"] = system.Rotation
 
         self.manage_scanlines(system, config)
 

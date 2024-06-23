5da48463af085c3e7bbf299d839cc0bb0cd858f5
David Barbion <davidb@230ruedubac.fr>
2024-06-23 23:37:15 +0200

    fix: add a new lightweight theme for gpicase2w
    The theme recalbox-goa-gpicase is a lightweight version of recalbox-goa2.
    This theme has been created to try to avoid the video bug explained here:
    https://github.com/raspberrypi/linux/issues/5780

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -5,16 +5,16 @@
 ################################################################################
 
 ifneq ($(GITLAB_TOKEN_THEMES),)
-RECALBOX_THEMES_VERSION = 906464a583b522312e4a3ff8ca15ed1444b7901d
+RECALBOX_THEMES_VERSION = 6ade73382648e87ce0a146fb14ee895b2eab5c9f
 RECALBOX_THEMES_SITE = https://gitlab-ci-token:$(GITLAB_TOKEN_THEMES)@gitlab.com/recalbox/recalbox-themes-prime
 else
-RECALBOX_THEMES_VERSION = 9a1279f1a0493aabf9e1d27686de2fb35aedf7e7
+RECALBOX_THEMES_VERSION = 6ade73382648e87ce0a146fb14ee895b2eab5c9f
 RECALBOX_THEMES_SITE = https://gitlab.com/recalbox/recalbox-themes
 endif
 RECALBOX_THEMES_SITE_METHOD = git
 RECALBOX_THEMES_LICENSE = CC-BY-NC-ND-4.0
 
-define RECALBOX_THEMES_INSTALL_TARGET_CMDS
+define RECALBOX_THEMES_COMMON
 	mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 	cp -r $(@D)/themes/recalbox-next \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
@@ -23,5 +23,15 @@ define RECALBOX_THEMES_INSTALL_TARGET_CMDS
 	cp -r $(@D)/themes/recalbox-240p \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
+
+define RECALBOX_THEMES_GPICASE2W
+	cp -r $(@D)/themes/recalbox-goa-gpicase \
+		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
+endef
+
+RECALBOX_THEMES_INSTALL_TARGET_CMDS = $(RECALBOX_THEMES_COMMON)
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPIZERO2),y)
+RECALBOX_THEMES_INSTALL_TARGET_CMDS += && $(RECALBOX_THEMES_GPICASE2W)
+endif
 
 $(eval $(generic-package))

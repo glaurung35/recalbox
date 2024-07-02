e66368ec0b7414252817c4a1548a42ea818ce02a
Adrien Beudin <beudbeud@beudibox.fr>
2024-07-02 15:06:35 +0200

    fix: reduce volume for flycast core

--- projects/configgen/configgen/generators/libretro/libretroGenerator.py
+++ projects/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -435,6 +435,12 @@ class LibretroGenerator(Generator):
             retroarchConfig.setString(option[0], option[1])
         retroarchConfig.saveFile()
 
+        if system.Core == "flycast" or system.Core == "flycast-next":
+            retroarchConfig.setString("audio_volume", "-2.0")
+        else:
+            retroarchConfig.setString("audio_volume", "0.0")
+        retroarchConfig.saveFile()
+
         commandArgs = configuration.getCommandLineArguments(retroarchConfig, coreConfig)
 
         return configuration.getRetroarchConfigurationFileName(), \

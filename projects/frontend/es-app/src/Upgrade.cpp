4833c127366c4474fc5575e7340ee7dce28e7c6f
Bkg2k <bkg2k9@gmail.com>
2024-07-09 08:35:43 +0200

    fix(frontend): fix multiple update popups

--- projects/frontend/es-app/src/Upgrade.cpp
+++ projects/frontend/es-app/src/Upgrade.cpp
@@ -13,8 +13,6 @@
 #include "RecalboxConf.h"
 #include "utils/locale/LocaleHelper.h"
 #include "recalbox/RecalboxSystem.h"
-#include "guis/GuiMsgBox.h"
-#include "guis/GuiMsgBoxScroll.h"
 #include "guis/GuiUpdateRecalbox.h"
 #include <patreon/PatronInfo.h>
 #include <guis/GuiInfoPopup.h>
@@ -24,6 +22,8 @@ String Upgrade::mRemoteVersion;
 String Upgrade::mLocalVersion;
 String Upgrade::mRemoteReleaseNote;
 String Upgrade::mLocalReleaseNote;
+
+Upgrade::UpdatePopup* Upgrade::UpdatePopup::mInstance = nullptr;
 
 Upgrade::Upgrade(WindowManager& window)
   : mWindow(window)
@@ -120,13 +120,7 @@ void Upgrade::ReceiveSyncMessage()
 
   // Messagebox
   if (!mMessageBoxMessage.empty())
-  {
-    //mWindow.displayScrollMessage(_("AN UPDATE IS AVAILABLE FOR YOUR RECALBOX"), mMessageBoxMessage, false);
-    Gui* gui = new GuiMsgBoxScroll(mWindow, _("AN UPDATE IS AVAILABLE FOR YOUR RECALBOX"), mMessageBoxMessage, _("LATER"), nullptr, _("UPDATE NOW"),
-                                   [this] { mWindow.pushGui(new GuiUpdateRecalbox(mWindow, TarUrl(), ImageUrl(), HashUrl(), NewVersion())); },
-                                   String::Empty, nullptr, TextAlignment::Left);
-    mWindow.pushGui(gui);
-  }
+    UpdatePopup::Show(&mWindow, mMessageBoxMessage);
 }
 
 String Upgrade::GetDomainName()

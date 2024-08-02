2211ad681f6443cd17848e2872e7d0a4f0dbf8ee
David Barbion <davidb@230ruedubac.fr>
2024-08-03 00:57:18 +0200

    chore(buildroot): remove sdl_sound in profit of sdl2_sound
    Dosbox is patched to make it compatible with sdl2_sound

--- external.mk
+++ external.mk
@@ -1,3 +1,4 @@
 include $(BR2_EXTERNAL_RECALBOX_PATH)/package/download-packages/download-packages.mk
 include $(BR2_EXTERNAL_RECALBOX_PATH)/package/compiler-commons/compiler-commons.mk
 include $(sort $(wildcard $(BR2_EXTERNAL_RECALBOX_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_RECALBOX_PATH)/package/libraries/*/*.mk))

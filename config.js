cabc2ffdbdc80721252df939efb676e4c1630fc2
Adrien Beudin <beudbeud@beudibox.fr>
2024-08-11 21:57:42 +0200

    feat: add renovate

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  endpoint: 'https://gitlab.com/api/v4',
+  hostRules: [],
+  platform: 'gitlab',
+  username: 'renovate-bot',
+  gitAuthor: 'Renovate Bot <renovate@recalbox.com>',
+  autodiscover: false,
+};

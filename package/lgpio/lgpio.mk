28d38b9d18e5469f0ac2c7f34a141f588dd04831
Adrien Beudin <beudbeud@beudibox.fr>
2024-09-11 10:17:12 +0200

    fix: lint

--- package/lgpio/lgpio.mk
+++ package/lgpio/lgpio.mk
@@ -7,6 +7,6 @@
 LGPIO_VERSION = 746f0df43774175090b93abcc860b6733eefc09b
 LGPIO_SITE = $(call github,joan2937,lg,$(LGPIO_VERSION))
 LGPIO_LICENSE = unlicense.org
-LGPIO_DEPENDENCIES = 
+LGPIO_DEPENDENCIES =
 
 $(eval $(generic-package))

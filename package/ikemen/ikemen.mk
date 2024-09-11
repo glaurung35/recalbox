d91607c9d787c5d427546d902c13ba5860d3d7e2
Adrien Beudin <beudbeud@beudibox.fr>
2024-09-11 22:30:29 +0200

    feat: add mugen

---
+++ package/ikemen/ikemen.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# ikemen
+#
+################################################################################
+
+IKEMEN_VERSION = 02a078d6bf189ff56bbcbbbc8dbfd81753692c56
+IKEMEN_SITE = https://github.com/ikemen-engine/Ikemen-GO.git
+IKEMEN_LICENSE = MIT
+IKEMEN_LICENSE_FILES = License.txt
+IKEMEN_SITE_METHOD = git
+IKEMEN_GIT_SUBMODULES = YES
+
+IKEMEN_DEPENDENCIES = libgtk3 mesa3d openal libglfw
+
+
+HOST_GO_COMMON_ENV = GOFLAGS=-mod=mod \
+		     GO111MODULE=on \
+		     GOROOT="$(HOST_GO_ROOT)" \
+		     GOPATH="$(HOST_GO_GOPATH)" \
+		     GOCACHE="$(HOST_GO_TARGET_CACHE)" \
+		     GOMODCACHE="$(@D)" \
+		     GOFLAGS="-modcacherw" \
+		     PATH=$(BR_PATH) \
+		     GOBIN= \
+		     CGO_ENABLED=$(HOST_GO_CGO_ENABLED)
+
+define IKEMEN_BUILD_CMDS
+	$(HOST_GO_TARGET_ENV) $(MAKE) -C $(@D) -f Makefile Ikemen_GO_Linux
+endef
+
+define IKEMEN_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D $(@D)/bin/Ikemen_GO_Linux $(TARGET_DIR)/usr/bin/ikemen
+endef
+
+$(eval $(golang-package))

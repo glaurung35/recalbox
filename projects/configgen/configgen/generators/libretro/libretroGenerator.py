9259a964fcd096d3d3dcc672c4edf21bed27b517
Adrien Beudin <beudbeud@beudibox.fr>
2024-09-21 20:56:07 +0200

    feat: add remap file

--- projects/configgen/configgen/generators/libretro/libretroGenerator.py
+++ projects/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -379,6 +379,23 @@ class LibretroGenerator(Generator):
             }
         return {"fbneo-cpu-speed-adjust": '"100%"'}
 
+    @staticmethod
+    def createRemapConfig(system: Emulator, rom: str, config: keyValueSettings):
+        remapConfig = keyValueSettings(recalboxFiles.retroarchRemapCommon, True)
+        remapConfig.loadFile(True)
+        remapConfig.clear()
+        remapConfigDevice = config.getOptionByRegex('input_libretro_device_p[1-8]$')
+
+        for option in remapConfigDevice.items():
+            remapConfig.setString(option[0], '"' + option[1] + '"')
+
+        for i in range(1, 9):
+            value = config.getInt("input_player{}_analog_dpad_mode".format(i), 0)
+            remapConfig.setString("input_player{}_analog_dpad_mode".format(i), '"' + str(value) + '"')
+            remapConfig.setString("input_remap_port_p{}".format(i), '"' + str(i - 1) + '"' )
+
+        remapConfig.saveFile()
+
 
     # recalbox-crt-options.cfg options
     # Create configuration file
@@ -443,11 +460,24 @@ class LibretroGenerator(Generator):
             retroarchConfig.setString(option[0], option[1])
         retroarchConfig.saveFile()
 
+        # Disable notification when use common remap file
+        retroarchConfig.setString("notification_show_remap_load", "false")
+        coreCapitalize = system.Core.capitalize()
+        romName = os.path.splitext(os.path.basename(rom))[0]
+        if (os.path.isfile("{}/{}/{}.rmp".format(recalboxFiles.retroarchRemap, coreCapitalize, coreCapitalize))
+                or os.path.isfile("{}/{}/{}.rmp".format(recalboxFiles.retroarchRemap, coreCapitalize, system.Name))
+                or os.path.isfile("{}/{}/{}.rmp".format(recalboxFiles.retroarchRemap, coreCapitalize, romName))):
+            retroarchConfig.setString("notification_show_remap_load", "true")
+
+        # Reduce volume for flycast
         if system.Core == "flycast" or system.Core == "flycast-next":
             retroarchConfig.setString("audio_volume", "-2.0")
         else:
             retroarchConfig.setString("audio_volume", "0.0")
         retroarchConfig.saveFile()
+
+        # Create remap file
+        LibretroGenerator.createRemapConfig(system, rom, retroarchConfig)
 
         commandArgs = configuration.getCommandLineArguments(retroarchConfig, coreConfig)
 

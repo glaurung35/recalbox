f8b0b5045c8a619adc4a17c6a934228e1d3c586d
David Barbion <davidb@230ruedubac.fr>
2024-09-29 21:49:43 +0200

    feat: bump rpi-utils to 193d1bec1c6db7e29b7ac4732e7bb396fbdd843d
    This brings GCC14 compatibility.
    One additional patch is still needed to cover this error:
    ./vclog/vclog.c:211:34: error: argument 2 value '4294967288'
    exceeds maximum object size 2147483647 [-Werror=alloc-size-larger-than=]

--- package/rpi-utils/rpi-utils.mk
+++ package/rpi-utils/rpi-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_UTILS_VERSION = d4f8fb5
+RPI_UTILS_VERSION = 193d1bec1c6db7e29b7ac4732e7bb396fbdd843d
 RPI_UTILS_SITE = $(call github,raspberrypi,utils,$(RPI_UTILS_VERSION))
 RPI_UTILS_LICENSE = BSD-3-Clause
 RPI_UTILS_LICENSE_FILES = LICENCE
